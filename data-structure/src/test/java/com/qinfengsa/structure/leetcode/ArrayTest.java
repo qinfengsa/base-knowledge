cddb93c4f116b990e32ef9d09ea77e58cc590d30
wangheng <42572617+qinfengsama@users.noreply.github.com>
2020-05-15 07:24:15 +0800

    2020-5-14 LeetCode刷题记录 187. 重复的DNA序列 560. 和为K的子数组 442. 数组中重复的数据

--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/ArrayTest.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/ArrayTest.java
@@ -10747,5 +10747,51 @@ public class ArrayTest {
         return list;
     }
 
+
+    /**
+     * 442. 数组中重复的数据
+     * 给定一个整数数组 a，其中1 ≤ a[i] ≤ n （n为数组长度）, 其中有些元素出现两次而其他元素出现一次。
+     *
+     * 找到所有出现两次的元素。
+     *
+     * 你可以不用到任何额外空间并在O(n)时间复杂度内解决这个问题吗？
+     *
+     * 示例：
+     *
+     * 输入:
+     * [4,3,2,7,8,2,3,1]
+     *
+     * 输出:
+     * [2,3]
+     * @param nums
+     * @return
+     */
+    public List<Integer> findDuplicates(int[] nums) {
+        // 1 ≤ a[i] ≤ n
+        // Arrays.sort(nums);
+       /* int[] arrays = new int[nums.length];
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            if ( arrays[num - 1] == 1) {
+                list.add(num);
+            }
+            arrays[num]++;
+        }*/
+
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            if (num < 0) {
+                num += nums.length + 1;
+            }
+            if (nums[num - 1] >= 0) {
+                nums[num - 1] -= nums.length + 1;
+            } else {
+                list.add(num);
+            }
+
+        }
+        return list;
+    }
+
 }
 
--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/MyHashTest.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/MyHashTest.java
@@ -2638,12 +2638,67 @@ public class MyHashTest {
      * @return
      */
     public List<String> findRepeatedDnaSequences(String s) {
-        List<String> result = new ArrayList<>();
+        Set<String> result = new HashSet<>();
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < s.length() - 10; i++) {
+            String str = s.substring(i,i + 10);
+            if (set.contains(str)) {
+                result.add(str);
+            }
+            set.add(str);
+        }
 
 
 
 
-        return result;
+        return new ArrayList<>(result);
+    }
+
+
+    /**
+     * 560. 和为K的子数组
+     * 给定一个整数数组和一个整数 k，你需要找到该数组中和为 k 的连续的子数组的个数。
+     *
+     * 示例 1 :
+     *
+     * 输入:nums = [1,1,1], k = 2
+     * 输出: 2 , [1,1] 与 [1,1] 为两种不同的情况。
+     * 说明 :
+     *
+     * 数组的长度为 [1, 20,000]。
+     * 数组中元素的范围是 [-1000, 1000] ，且整数 k 的范围是 [-1e7, 1e7]。
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int subarraySum(int[] nums, int k) {
+        int count = 0,sum = 0;
+        Map<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+            int n = map.getOrDefault(sum,0);
+            map.put(sum, n + 1);
+        }
+
+
+        return count;
+        /*// 滑动窗口
+        int left = 0,right = 0;
+        int sum = 0;
+        while (sum < k && right < nums.length) {
+            sum += nums[right++];
+        }
+        if (sum == k) {
+            count++;
+        }
+        for (int i = right; i < nums.length; i++) {
+            sum +=
+        }*/
+
     }
 
 }
