fe5459616c78ffae47e082f8f1103279f644871a
wangheng <wangheng@winwin-inc.com>
2020-05-27 17:24:41 +0800

    2020-5-26 LeetCode刷题记录 503. 下一个更大元素 II 515. 在每个树行中找最大值 413. 等差数列划分

--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/TreeTest.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/TreeTest.java
@@ -3899,4 +3899,48 @@ public class TreeTest {
 
         return left;
     }
+
+
+    /**
+     * 515. 在每个树行中找最大值
+     * 您需要在二叉树的每一行中找到最大的值。
+     *
+     * 示例：
+     *
+     * 输入:
+     *
+     *           1
+     *          / \
+     *         3   2
+     *        / \   \
+     *       5   3   9
+     *
+     * 输出: [1, 3, 9]
+     * @param root
+     * @return
+     */
+    public List<Integer> largestValues(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.offer(root);
+        List<Integer> list = new ArrayList<>();
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int max = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                max = Math.max(max,node.val);
+                if (Objects.nonNull(node.left)) {
+                    queue.offer(node.left);
+                }
+                if (Objects.nonNull(node.right)) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+
+        return list;
+
+    }
 }
