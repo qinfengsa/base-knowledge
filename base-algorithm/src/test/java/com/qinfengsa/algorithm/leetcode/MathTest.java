cf65a5d5945d4d2140fd7a3c1d003a8a9afcf205
wangheng <wangheng@winwin-inc.com>
2020-06-22 21:56:50 +0800

    2020-6-22 LeetCode刷题记录 435. 无重叠区间 456. 132模式 462. 最少移动次数使数组元素相等 II

--- base-algorithm/src/test/java/com/qinfengsa/algorithm/leetcode/MathTest.java
+++ base-algorithm/src/test/java/com/qinfengsa/algorithm/leetcode/MathTest.java
@@ -5039,4 +5039,40 @@ public class MathTest {
         }
         return num;
     }
+
+    @Test
+    public void minMoves2() {
+        int[] nums = {1, 2, 3, 4, 8};
+        logResult(minMoves2(nums));
+    }
+
+    /**
+     * 462. 最少移动次数使数组元素相等 II
+     *
+     * <p>给定一个非空整数数组，找到使所有数组元素相等所需的最小移动数，其中每次移动可将选定的一个元素加1或减1。 您可以假设数组的长度最多为10000。
+     *
+     * <p>例如:
+     *
+     * <p>输入: [1,2,3]
+     *
+     * <p>输出: 2
+     *
+     * <p>说明： 只有两个动作是必要的（记得每一步仅可使其中一个元素加1或减1）：
+     *
+     * <p>[1,2,3] => [2,2,3] => [2,2,2]
+     *
+     * @param nums
+     * @return
+     */
+    public int minMoves2(int[] nums) {
+        int move = 0;
+        Arrays.sort(nums);
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            move += (nums[right] - nums[left]);
+            left++;
+            right--;
+        }
+        return move;
+    }
 }
--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/StackTest.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/StackTest.java
@@ -776,4 +776,66 @@ public class StackTest {
 
         return result;
     }
+
+    @Test
+    public void find132pattern() {
+        int[] nums = {-1, 3, 2, 0};
+        logResult(find132pattern(nums));
+    }
+
+    /**
+     * 456. 132模式
+     *
+     * <p>给定一个整数序列：a1, a2, ..., an，一个132模式的子序列 ai, aj, ak 被定义为：当 i < j < k 时，ai < ak <
+     * aj。设计一个算法，当给定有 n 个数字的序列时，验证这个序列中是否含有132模式的子序列。
+     *
+     * <p>注意：n 的值小于15000。
+     *
+     * <p>示例1:
+     *
+     * <p>输入: [1, 2, 3, 4]
+     *
+     * <p>输出: False
+     *
+     * <p>解释: 序列中不存在132模式的子序列。 示例 2:
+     *
+     * <p>输入: [3, 1, 4, 2]
+     *
+     * <p>输出: True
+     *
+     * <p>解释: 序列中有 1 个132模式的子序列： [1, 4, 2]. 示例 3:
+     *
+     * <p>输入: [-1, 3, 2, 0]
+     *
+     * <p>输出: True
+     *
+     * <p>解释: 序列中有 3 个132模式的的子序列: [-1, 3, 2], [-1, 3, 0] 和 [-1, 2, 0].
+     *
+     * @param nums
+     * @return
+     */
+    public boolean find132pattern(int[] nums) {
+        // 最小值列表
+        int[] minNums = new int[nums.length];
+        minNums[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            minNums[i] = Math.min(minNums[i - 1], nums[i]);
+        }
+
+        Deque<Integer> stack = new LinkedList<>();
+        stack.push(nums[nums.length - 1]);
+        for (int i = nums.length - 2; i > 0; i--) {
+            if (nums[i] > minNums[i]) {
+                while (!stack.isEmpty() && stack.peek() <= minNums[i]) {
+                    stack.pop();
+                }
+                if (!stack.isEmpty() && stack.peek() < nums[i]) {
+                    return true;
+                }
+
+                stack.push(nums[i]);
+            }
+        }
+        return false;
+    }
 }
