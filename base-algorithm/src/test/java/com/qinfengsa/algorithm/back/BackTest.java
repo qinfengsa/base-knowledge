146848c3766a763458d26ce2d97620976c09c8af
wangheng <wangheng@winwin-inc.com>
2020-07-08 16:46:25 +0800

    2020-7-8 LeetCode刷题记录 638. 大礼包 646. 最长数对链 647. 回文子串

--- base-algorithm/src/test/java/com/qinfengsa/algorithm/back/BackTest.java
+++ base-algorithm/src/test/java/com/qinfengsa/algorithm/back/BackTest.java
@@ -2301,4 +2301,105 @@ public class BackTest {
             }
         }
     }
+
+    /**
+     * 638. 大礼包
+     *
+     * <p>在LeetCode商店中， 有许多在售的物品。
+     *
+     * <p>然而，也有一些大礼包，每个大礼包以优惠的价格捆绑销售一组物品。
+     *
+     * <p>现给定每个物品的价格，每个大礼包包含物品的清单，以及待购物品清单。请输出确切完成待购清单的最低花费。
+     *
+     * <p>每个大礼包的由一个数组中的一组数据描述，最后一个数字代表大礼包的价格，其他数字分别表示内含的其他种类物品的数量。
+     *
+     * <p>任意大礼包可无限次购买。
+     *
+     * <p>示例 1:
+     *
+     * <p>输入: [2,5], [[3,0,5],[1,2,10]], [3,2] 输出: 14 解释: 有A和B两种物品，价格分别为¥2和¥5。
+     * 大礼包1，你可以以¥5的价格购买3A和0B。 大礼包2， 你可以以¥10的价格购买1A和2B。 你需要购买3个A和2个B，
+     * 所以你付了¥10购买了1A和2B（大礼包2），以及¥4购买2A。 示例 2:
+     *
+     * <p>输入: [2,3,4], [[1,1,0,4],[2,2,1,9]], [1,2,1] 输出: 11 解释: A，B，C的价格分别为¥2，¥3，¥4.
+     * 你可以用¥4购买1A和1B，也可以用¥9购买2A，2B和1C。 你需要买1A，2B和1C，所以你付了¥4买了1A和1B（大礼包1），以及¥3购买1B， ¥4购买1C。
+     * 你不可以购买超出待购清单的物品，尽管购买大礼包2更加便宜。 说明:
+     *
+     * <p>最多6种物品， 100种大礼包。 每种物品，你最多只需要购买6个。 你不可以购买超出待购清单的物品，即使更便宜。
+     *
+     * @param price
+     * @param special
+     * @param needs
+     * @return
+     */
+    public int shoppingOffers(
+            List<Integer> price, List<List<Integer>> special, List<Integer> needs) {
+        minShoppingOffersResult = Integer.MAX_VALUE;
+        shoppingOffersSum = 0;
+        backShopping(0, price, special, needs);
+        return minShoppingOffersResult;
+    }
+
+    private int minShoppingOffersResult;
+
+    private int shoppingOffersSum;
+
+    private void backShopping(
+            int start, List<Integer> price, List<List<Integer>> special, List<Integer> needs) {
+        if (!validNeeds(needs)) {
+            return;
+        }
+
+        int tempSum = shoppingOffersSum;
+        for (int i = 0; i < needs.size(); ++i) {
+            shoppingOffersSum += needs.get(i) * price.get(i);
+        }
+        minShoppingOffersResult = Math.min(minShoppingOffersResult, shoppingOffersSum);
+        shoppingOffersSum = tempSum;
+        for (int i = start; i < special.size(); ++i) {
+            List<Integer> curSpecial = special.get(i);
+            int specialNum = calSpecialNum(curSpecial, needs);
+            if (specialNum != 0) {
+                for (int j = 1; j <= specialNum; ++j) {
+                    List<Integer> tempNeeds = new ArrayList<>(needs);
+                    for (int k = 0; k < needs.size(); ++k)
+                        needs.set(k, needs.get(k) - curSpecial.get(k) * j);
+
+                    shoppingOffersSum += curSpecial.get(needs.size()) * j;
+                    backShopping(i + 1, price, special, needs);
+                    needs = tempNeeds;
+                    shoppingOffersSum = tempSum;
+                }
+            }
+        }
+    }
+
+    private int calSpecialNum(List<Integer> item, List<Integer> needs) {
+        int max = Integer.MAX_VALUE;
+        for (int i = 0; i < needs.size(); ++i) {
+            int curNeed = needs.get(i), curItem = item.get(i);
+            if (curNeed < curItem) return 0;
+
+            if (curItem != 0) max = Math.min(curNeed / curItem, max);
+        }
+        return max;
+    }
+
+    private boolean needsZero(List<Integer> needs) {
+        for (int need : needs) {
+            if (need != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean validNeeds(List<Integer> needs) {
+        for (int need : needs) {
+            if (need < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- base-algorithm/src/test/java/com/qinfengsa/algorithm/dynamic/DynamicPlanTest.java
+++ base-algorithm/src/test/java/com/qinfengsa/algorithm/dynamic/DynamicPlanTest.java
@@ -3913,4 +3913,100 @@ public class DynamicPlanTest {
         logResult(side);
         return max;
     }
+
+    @Test
+    public void findLongestChain() {
+        int[][] pairs = {{3, 4}, {2, 3}, {1, 2}};
+        logResult(findLongestChain(pairs));
+    }
+
+    /**
+     * 646. 最长数对链
+     *
+     * <p>给出 n 个数对。 在每一个数对中，第一个数字总是比第二个数字小。
+     *
+     * <p>现在，我们定义一种跟随关系，当且仅当 b < c 时，数对(c, d) 才可以跟在 (a, b) 后面。我们用这种形式来构造一个数对链。
+     *
+     * <p>给定一个对数集合，找出能够形成的最长数对链的长度。你不需要用到所有的数对，你可以以任何顺序选择其中的一些数对来构造。
+     *
+     * <p>示例 :
+     *
+     * <p>输入: [[1,2], [2,3], [3,4]] 输出: 2 解释: 最长的数对链是 [1,2] -> [3,4] 注意：
+     *
+     * <p>给出数对的个数在 [1, 1000] 范围内。
+     *
+     * @param pairs
+     * @return
+     */
+    public int findLongestChain(int[][] pairs) {
+        int result = 0;
+        int len = pairs.length;
+        int[] dp = new int[pairs.length];
+        Arrays.sort(pairs, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        dp[0] = 1;
+        for (int i = 1; i < len; i++) {
+            int num = pairs[i][0];
+            int index = i - 1;
+            while (index >= 0 && pairs[index][1] >= num) {
+                index--;
+            }
+            if (index >= 0) {
+                dp[i] = dp[index] + 1;
+            } else {
+                dp[i] = 1;
+            }
+            result = Math.max(dp[i], result);
+        }
+        log.debug("dp:{}", dp);
+
+        return result;
+    }
+
+    @Test
+    public void countSubstrings() {
+        String s = "aaaaa";
+        logResult(countSubstrings(s));
+    }
+
+    /**
+     * 647. 回文子串
+     *
+     * <p>给定一个字符串，你的任务是计算这个字符串中有多少个回文子串。
+     *
+     * <p>具有不同开始位置或结束位置的子串，即使是由相同的字符组成，也会被计为是不同的子串。
+     *
+     * <p>示例 1:
+     *
+     * <p>输入: "abc" 输出: 3 解释: 三个回文子串: "a", "b", "c". 示例 2:
+     *
+     * <p>输入: "aaa" 输出: 6 说明: 6个回文子串: "a", "a", "a", "aa", "aa", "aaa". 注意:
+     *
+     * <p>输入的字符串长度不会超过1000。
+     *
+     * @param s
+     * @return
+     */
+    public int countSubstrings(String s) {
+        // dp[i][j] 表示 位置 i ~ j 是否回文子串
+        // dp[i][j] = dp[i - 1][j - 1]
+        int len = s.length();
+        boolean[][] dp = new boolean[len][len];
+        int result = 0;
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+            result++;
+        }
+        for (int i = len - 1; i >= 0; i--) {
+            for (int j = i + 1; j < len; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (j - i == 1 || dp[i + 1][j - 1]) {
+                        dp[i][j] = true;
+                        result++;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
 }
