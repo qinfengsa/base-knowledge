7f881beab5ac1721cb42c32c7d9be37dad343cf6
qinfengsa <15800012123@163.com>
2020-07-16 16:40:08 +0800

    2020-7-16 LeetCode刷题记录 785. 判断二分图 1432. 改变一个整数能得到的最大差值 1433. 检查一个字符串是否可以打破另一个字符串

--- base-algorithm/src/test/java/com/qinfengsa/algorithm/leetcode/MathTest.java
+++ base-algorithm/src/test/java/com/qinfengsa/algorithm/leetcode/MathTest.java
@@ -5570,4 +5570,93 @@ public class MathTest {
             }
         }
     }
+
+    @Test
+    public void maxDiff() {
+        int num = 111;
+        logResult(maxDiff(num));
+    }
+
+    /**
+     * 1432. 改变一个整数能得到的最大差值
+     *
+     * <p>给你一个整数 num 。你可以对它进行如下步骤恰好 两次 ：
+     *
+     * <p>选择一个数字 x (0 <= x <= 9). 选择另一个数字 y (0 <= y <= 9) 。数字 y 可以等于 x 。 将 num 中所有出现 x 的数位都用 y 替换。
+     * 得到的新的整数 不能 有前导 0 ，得到的新整数也 不能 是 0 。 令两次对 num 的操作得到的结果分别为 a 和 b 。
+     *
+     * <p>请你返回 a 和 b 的 最大差值 。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：num = 555 输出：888 解释：第一次选择 x = 5 且 y = 9 ，并把得到的新数字保存在 a 中。 第二次选择 x = 5 且 y = 1
+     * ，并把得到的新数字保存在 b 中。 现在，我们有 a = 999 和 b = 111 ，最大差值为 888 示例 2：
+     *
+     * <p>输入：num = 9 输出：8 解释：第一次选择 x = 9 且 y = 9 ，并把得到的新数字保存在 a 中。 第二次选择 x = 9 且 y = 1 ，并把得到的新数字保存在
+     * b 中。 现在，我们有 a = 9 和 b = 1 ，最大差值为 8 示例 3：
+     *
+     * <p>输入：num = 123456 输出：820000 示例 4：
+     *
+     * <p>输入：num = 10000 输出：80000 示例 5：
+     *
+     * <p>输入：num = 9288 输出：8700
+     *
+     * <p>提示：
+     *
+     * <p>1 <= num <= 10^8
+     *
+     * @param num
+     * @return
+     */
+    public int maxDiff(int num) {
+        char[] chars = String.valueOf(num).toCharArray();
+        char[] chars2 = chars.clone();
+        int len = chars.length;
+        // 找到第一个不是9的数字 变成9
+        int index = 0;
+        while (index < len && chars[index] == '9') {
+            index++;
+        }
+        if (index < len) {
+            char c = chars[index];
+            for (int i = index; i < len; i++) {
+                if (chars[i] == c) {
+                    chars[i] = '9';
+                }
+            }
+        }
+        int max = Integer.parseInt(new String(chars));
+        log.debug("index:{}", index);
+        log.debug("chars:{}", chars);
+        log.debug("max:{}", max);
+        // 第一个数字不为1 修改为1
+        if (chars2[0] != '1') {
+            char c = chars2[0];
+
+            for (int i = 0; i < len; i++) {
+                if (chars2[i] == c) {
+                    chars2[i] = '1';
+                }
+            }
+        }
+
+        // 第一个数字为1 找到第一个不是0 或 1的数字 变成0
+        else {
+            index = 1;
+            while (index < len && (chars2[index] == '0' || chars2[index] == '1')) {
+                index++;
+            }
+            if (index < len) {
+                char c = chars2[index];
+                for (int i = index; i < len; i++) {
+                    if (chars2[i] == c) {
+                        chars2[i] = '0';
+                    }
+                }
+            }
+        }
+        int min = Integer.parseInt(new String(chars2));
+        log.debug("min:{}", min);
+        return max - min;
+    }
 }
