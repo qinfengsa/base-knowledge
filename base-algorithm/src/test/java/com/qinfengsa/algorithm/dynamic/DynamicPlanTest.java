c30a779472dd079adefedcdb6a0e4e1d369e833b
qinfengsa <15800012123@163.com>
2020-07-25 13:54:17 +0800

    2020-7-25 LeetCode刷题记录 764. 最大加号标志 769. 最多能完成排序的块 781. 森林中的兔子

--- base-algorithm/src/test/java/com/qinfengsa/algorithm/dynamic/DynamicPlanTest.java
+++ base-algorithm/src/test/java/com/qinfengsa/algorithm/dynamic/DynamicPlanTest.java
@@ -4323,4 +4323,156 @@ public class DynamicPlanTest {
         logResult(dp);
         return dp[len1][len2];
     }
+
+    @Test
+    public void orderOfLargestPlusSign() {
+        int N = 1;
+        int[][] mines = {{0, 0}};
+        logResult(orderOfLargestPlusSign(N, mines));
+    }
+
+    /**
+     * 764. 最大加号标志
+     *
+     * <p>在一个大小在 (0, 0) 到 (N-1, N-1) 的2D网格 grid 中，除了在 mines 中给出的单元为 0，其他每个单元都是 1。网格中包含 1
+     * 的最大的轴对齐加号标志是多少阶？返回加号标志的阶数。如果未找到加号标志，则返回 0。
+     *
+     * <p>一个 k" 阶由 1 组成的“轴对称”加号标志具有中心网格 grid[x][y] = 1 ，以及4个从中心向上、向下、向左、向右延伸，长度为 k-1，由 1 组成的臂。下面给出
+     * k" 阶“轴对称”加号标志的示例。注意，只有加号标志的所有网格要求为 1，别的网格可能为 0 也可能为 1。
+     *
+     * <p>k 阶轴对称加号标志示例:
+     *
+     * <p>阶 1: 000 010 000
+     *
+     * <p>阶 2: 00000 00100 01110 00100 00000
+     *
+     * <p>阶 3: 0000000 0001000 0001000 0111110 0001000 0001000 0000000
+     *
+     * <p>示例 1：
+     *
+     * <p>输入: N = 5, mines = [[4, 2]] 输出: 2 解释:
+     *
+     * <p>11111 11111 11111 11111 11011
+     *
+     * <p>在上面的网格中，最大加号标志的阶只能是2。一个标志已在图中标出。
+     *
+     * <p>示例 2：
+     *
+     * <p>输入: N = 2, mines = [] 输出: 1 解释:
+     *
+     * <p>11 11
+     *
+     * <p>没有 2 阶加号标志，有 1 阶加号标志。
+     *
+     * <p>示例 3：
+     *
+     * <p>输入: N = 1, mines = [[0, 0]] 输出: 0 解释:
+     *
+     * <p>0
+     *
+     * <p>没有加号标志，返回 0 。
+     *
+     * <p>提示：
+     *
+     * <p>整数N 的范围： [1, 500]. mines 的最大长度为 5000. mines[i] 是长度为2的由2个 [0, N-1] 中的数组成. (另外,使用 C, C++, 或者
+     * C# 编程将以稍小的时间限制进行​​判断.)
+     *
+     * @param N
+     * @param mines
+     * @return
+     */
+    public int orderOfLargestPlusSign(int N, int[][] mines) {
+
+        /*Set<Integer> banned = new HashSet();
+        int[][] dp = new int[N][N];
+
+        for (int[] mine : mines) {
+            banned.add(mine[0] * N + mine[1]);
+        }
+        int ans = 0, count;
+
+        for (int r = 0; r < N; ++r) {
+            count = 0;
+            for (int c = 0; c < N; ++c) {
+                count = banned.contains(r * N + c) ? 0 : count + 1;
+                dp[r][c] = count;
+            }
+
+            count = 0;
+            for (int c = N - 1; c >= 0; --c) {
+                count = banned.contains(r * N + c) ? 0 : count + 1;
+                dp[r][c] = Math.min(dp[r][c], count);
+            }
+        }
+
+        for (int c = 0; c < N; ++c) {
+            count = 0;
+            for (int r = 0; r < N; ++r) {
+                count = banned.contains(r * N + c) ? 0 : count + 1;
+                dp[r][c] = Math.min(dp[r][c], count);
+            }
+
+            count = 0;
+            for (int r = N - 1; r >= 0; --r) {
+                count = banned.contains(r * N + c) ? 0 : count + 1;
+                dp[r][c] = Math.min(dp[r][c], count);
+                ans = Math.max(ans, dp[r][c]);
+            }
+        }
+
+        return ans;*/
+
+        if (N == 0) {
+            return 0;
+        }
+
+        int[][] dp = new int[N][N];
+
+        int matrix[][] = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(matrix[i], 1);
+        }
+
+        for (int i = 0; i < mines.length; i++) {
+            matrix[mines[i][0]][mines[i][1]] = 0;
+        }
+        int count = 0;
+        // 求左、右
+        for (int i = 0; i < N; i++) {
+
+            // 求左臂并将值放入dp left->right
+            count = 0;
+            for (int j = 0; j < N; j++) {
+                count = matrix[i][j] == 1 ? count + 1 : 0;
+                dp[i][j] = count;
+            }
+
+            // 求右臂 并将当前dp与右臂之间的最小值放入dp right->left
+            count = 0;
+            for (int j = N - 1; j >= 0; j--) {
+                count = matrix[i][j] == 1 ? count + 1 : 0;
+                dp[i][j] = Math.min(dp[i][j], count);
+            }
+        }
+        int max = 0;
+        // 求上、下
+        for (int j = 0; j < N; j++) {
+            // 求上臂 并将当前dp与上臂间的最小值放入dp up->down
+            count = 0;
+            for (int i = 0; i < N; i++) {
+                count = matrix[i][j] == 1 ? count + 1 : 0;
+                dp[i][j] = Math.min(dp[i][j], count);
+            }
+            // 求下臂 并将当前dp与下臂间的最小值放入dp down->up
+            // 此时的整个dp数组中的最大值就是所求的k
+            count = 0;
+            for (int i = N - 1; i >= 0; i--) {
+                count = matrix[i][j] == 1 ? count + 1 : 0;
+                dp[i][j] = Math.min(dp[i][j], count);
+                max = Math.max(max, dp[i][j]);
+            }
+        }
+
+        return max;
+    }
 }
--- base-algorithm/src/test/java/com/qinfengsa/algorithm/leetcode/MathTest.java
+++ base-algorithm/src/test/java/com/qinfengsa/algorithm/leetcode/MathTest.java
@@ -5815,4 +5815,53 @@ public class MathTest {
         // 余数是奇数
         return (target & 1) == 0 ? k : k + 1 + k % 2;
     }
+
+    @Test
+    public void numRabbits() {
+        int[] answers = {10, 10, 10};
+        logResult(numRabbits(answers));
+    }
+
+    /**
+     * 781. 森林中的兔子
+     *
+     * <p>森林中，每个兔子都有颜色。其中一些兔子（可能是全部）告诉你还有多少其他的兔子和自己有相同的颜色。我们将这些回答放在 answers 数组里。
+     *
+     * <p>返回森林中兔子的最少数量。
+     *
+     * <p>示例: 输入: answers = [1, 1, 2] 输出: 5 解释: 两只回答了 "1" 的兔子可能有相同的颜色，设为红色。 之后回答了 "2"
+     * 的兔子不会是红色，否则他们的回答会相互矛盾。 设回答了 "2" 的兔子为蓝色。 此外，森林中还应有另外 2 只蓝色兔子的回答没有包含在数组中。 因此森林中兔子的最少数量是 5: 3
+     * 只回答的和 2 只没有回答的。
+     *
+     * <p>输入: answers = [10, 10, 10] 输出: 11
+     *
+     * <p>输入: answers = [] 输出: 0 说明:
+     *
+     * <p>answers 的长度最大为1000。 answers[i] 是在 [0, 999] 范围内的整数。
+     *
+     * @param answers
+     * @return
+     */
+    public int numRabbits(int[] answers) {
+        int[] count = new int[1000];
+        for (int x : answers) {
+            count[x]++;
+        }
+
+        int result = count[0];
+        for (int i = 1; i < 1000; i++) {
+            if (count[i] == 0) {
+                continue;
+            }
+            int div = count[i] / (i + 1);
+            int rem = count[i] % (i + 1);
+
+            result += div * (i + 1);
+            if (rem > 0) {
+                result += i + 1;
+            }
+        }
+
+        return result;
+    }
 }
