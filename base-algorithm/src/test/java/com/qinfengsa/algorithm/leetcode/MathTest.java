f72f14d13580dd4f16f4cb76e98eeea6b31827f9
qinfengsa <15800012123@163.com>
2020-07-27 19:37:47 +0800

    2020-7-27 LeetCode刷题记录 1523. 在区间范围内统计奇数数目 1524. 和为奇数的子数组数目

--- base-algorithm/src/test/java/com/qinfengsa/algorithm/leetcode/MathTest.java
+++ base-algorithm/src/test/java/com/qinfengsa/algorithm/leetcode/MathTest.java
@@ -5927,4 +5927,91 @@ public class MathTest {
 
         return count;
     }
+
+    @Test
+    public void numOfSubarrays() {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        logResult(numOfSubarrays(arr));
+    }
+
+    /**
+     * 1523. 在区间范围内统计奇数数目
+     *
+     * <p>给你两个非负整数 low 和 high 。请你返回 low 和 high 之间（包括二者）奇数的数目。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：low = 3, high = 7 输出：3 解释：3 到 7 之间奇数数字为 [3,5,7] 。 示例 2：
+     *
+     * <p>输入：low = 8, high = 10 输出：1 解释：8 到 10 之间奇数数字为 [9] 。
+     *
+     * <p>提示：
+     *
+     * <p>0 <= low <= high <= 10^9
+     *
+     * @param low
+     * @param high
+     * @return
+     */
+    public int countOdds(int low, int high) {
+        int count = 0;
+        int len = high - low + 1;
+        if ((len & 1) == 0) {
+            count = len >> 1;
+        } else {
+
+            count = len >> 1;
+            if ((low & 1) == 1) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * 1524. 和为奇数的子数组数目
+     *
+     * <p>给你一个整数数组 arr 。请你返回和为 奇数 的子数组数目。
+     *
+     * <p>由于答案可能会很大，请你将结果对 10^9 + 7 取余后返回。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：arr = [1,3,5] 输出：4 解释：所有的子数组为 [[1],[1,3],[1,3,5],[3],[3,5],[5]] 。 所有子数组的和为
+     * [1,4,9,3,8,5]. 奇数和包括 [1,9,3,5] ，所以答案为 4 。 示例 2 ：
+     *
+     * <p>输入：arr = [2,4,6] 输出：0 解释：所有子数组为 [[2],[2,4],[2,4,6],[4],[4,6],[6]] 。 所有子数组和为
+     * [2,6,12,4,10,6] 。 所有子数组和都是偶数，所以答案为 0 。 示例 3：
+     *
+     * <p>输入：arr = [1,2,3,4,5,6,7] 输出：16 示例 4：
+     *
+     * <p>输入：arr = [100,100,99,99] 输出：4 示例 5：
+     *
+     * <p>输入：arr = [7] 输出：1
+     *
+     * <p>提示：
+     *
+     * <p>1 <= arr.length <= 10^5 1 <= arr[i] <= 100
+     *
+     * @param arr
+     * @return
+     */
+    public int numOfSubarrays(int[] arr) {
+        int MOD = 1000000007;
+        // 0 偶数个数 1 奇数个数
+        int[] cnts = new int[2];
+        int cur = 0;
+        int result = 0;
+        cnts[0] = 1;
+        for (int num : arr) {
+            // 查看当前的前缀和 是奇数或偶数
+            cur = (cur + num) & 1;
+            // 结果加上前缀和相反的个数
+            result += cnts[1 - cur];
+            // 前缀和为cur的个数++
+            cnts[cur]++;
+            result %= MOD;
+        }
+        return result;
+    }
 }
