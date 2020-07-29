14a0252c737563ef8bedae09321e0a79dda91b4b
qinfengsa <15800012123@163.com>
2020-07-29 08:05:56 +0800

    2020-7-29 LeetCode刷题记录 1400. 构造 K 个回文字符串 1401. 圆和矩形是否有重叠

--- base-algorithm/src/test/java/com/qinfengsa/algorithm/leetcode/MathTest.java
+++ base-algorithm/src/test/java/com/qinfengsa/algorithm/leetcode/MathTest.java
@@ -6014,4 +6014,85 @@ public class MathTest {
         }
         return result;
     }
+
+    /**
+     * 1401. 圆和矩形是否有重叠
+     *
+     * <p>给你一个以 (radius, x_center, y_center) 表示的圆和一个与坐标轴平行的矩形 (x1, y1, x2, y2)，其中 (x1, y1)
+     * 是矩形左下角的坐标，(x2, y2) 是右上角的坐标。
+     *
+     * <p>如果圆和矩形有重叠的部分，请你返回 True ，否则返回 False 。
+     *
+     * <p>换句话说，请你检测是否 存在 点 (xi, yi) ，它既在圆上也在矩形上（两者都包括点落在边界上的情况）。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：radius = 1, x_center = 0, y_center = 0, x1 = 1, y1 = -1, x2 = 3, y2 = 1 输出：true
+     * 解释：圆和矩形有公共点 (1,0) 示例 2：
+     *
+     * <p>输入：radius = 1, x_center = 0, y_center = 0, x1 = -1, y1 = 0, x2 = 0, y2 = 1 输出：true 示例 3：
+     *
+     * <p>输入：radius = 1, x_center = 1, y_center = 1, x1 = -3, y1 = -3, x2 = 3, y2 = 3 输出：true 示例 4：
+     *
+     * <p>输入：radius = 1, x_center = 1, y_center = 1, x1 = 1, y1 = -3, x2 = 2, y2 = -1 输出：false
+     *
+     * <p>提示：
+     *
+     * <p>1 <= radius <= 2000 -10^4 <= x_center, y_center, x1, y1, x2, y2 <= 10^4 x1 < x2 y1 < y2
+     *
+     * @param radius
+     * @param x_center
+     * @param y_center
+     * @param x1
+     * @param y1
+     * @param x2
+     * @param y2
+     * @return
+     */
+    public boolean checkOverlap(
+            int radius, int x_center, int y_center, int x1, int y1, int x2, int y2) {
+        // 分为 3 种情况
+        // 1 圆心在矩形内,肯定相交
+        // 2 圆心在矩形的上下左右, 半径  >= 圆心到最近的边的距离 -> 相交
+        // 3 圆心在矩形的四个角, 半径 >= 圆心到最近的角的距离 -> 相交
+
+        if (x_center < x1) {
+            if (y_center > y2) {
+                // 左上
+                return radius * radius
+                        >= (x1 - x_center) * (x1 - x_center) + (y_center - y2) * (y_center - y2);
+            } else if (y_center < y1) {
+                // 左下
+                return radius * radius
+                        >= (x1 - x_center) * (x1 - x_center) + (y1 - y_center) * (y1 - y_center);
+            } else {
+                // 左
+                return radius >= x1 - x_center;
+            }
+        } else if (x_center > x2) {
+            if (y_center > y2) {
+                // 右上
+                return radius * radius
+                        >= (x_center - x2) * (x_center - x2) + (y_center - y2) * (y_center - y2);
+            } else if (y_center < y1) {
+                // 右下
+                return radius * radius
+                        >= (x_center - x2) * (x_center - x2) + (y1 - y_center) * (y1 - y_center);
+            } else {
+                // 右
+                return radius >= x_center - x2;
+            }
+        } else {
+            // 上
+            if (y_center > y2) {
+                return radius >= y_center - y2;
+            } else if (y_center < y1) {
+                // 下
+                return radius >= y1 - y_center;
+            } else {
+                // 在矩形内
+                return true;
+            }
+        }
+    }
 }
