ac03c841a231fbc22f67484598052b419abada09
qinfengsa <15800012123@163.com>
2020-07-31 20:56:55 +0800

    2020-7-31 LeetCode刷题记录 756. 金字塔转换矩阵 787. K 站中转内最便宜的航班 1079. 活字印刷

--- base-algorithm/src/test/java/com/qinfengsa/algorithm/back/BackTest.java
+++ base-algorithm/src/test/java/com/qinfengsa/algorithm/back/BackTest.java
@@ -2523,4 +2523,54 @@ public class BackTest {
     int total(int n) {
         return 3 * (1 << (n - 1));
     }
+
+    /**
+     * 1079. 活字印刷
+     *
+     * <p>你有一套活字字模 tiles，其中每个字模上都刻有一个字母 tiles[i]。返回你可以印出的非空字母序列的数目。
+     *
+     * <p>注意：本题中，每个活字字模只能使用一次。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入："AAB" 输出：8 解释：可能的序列为 "A", "B", "AA", "AB", "BA", "AAB", "ABA", "BAA"。 示例 2：
+     *
+     * <p>输入："AAABBC" 输出：188
+     *
+     * <p>提示：
+     *
+     * <p>1 <= tiles.length <= 7 tiles 由大写英文字母组成
+     *
+     * @param tiles
+     * @return
+     */
+    public int numTilePossibilities(String tiles) {
+        if (tiles.length() == 0) {
+            return 0;
+        }
+        char[] tilesChar = tiles.toCharArray();
+        Arrays.sort(tilesChar);
+        numTilePossibilitiesBack(tilesChar, 0, new boolean[tiles.length()]);
+        return numTileResult;
+    }
+
+    private int numTileResult;
+
+    private void numTilePossibilitiesBack(char[] tilesChar, int idx, boolean[] visited) {
+        if (idx == tilesChar.length) {
+            return;
+        }
+        for (int i = 0; i < tilesChar.length; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            if (i > 0 && tilesChar[i] == tilesChar[i - 1] && !visited[i - 1]) {
+                continue;
+            }
+            numTileResult++;
+            visited[i] = true;
+            numTilePossibilitiesBack(tilesChar, idx + 1, visited);
+            visited[i] = false;
+        }
+    }
 }
--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/GraphTest.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/GraphTest.java
@@ -1505,4 +1505,76 @@ public class GraphTest {
                     dfsNetworkDelayTime(map, k, time + v, visited);
                 });
     }
+
+    @Test
+    public void findCheapestPrice() {
+        int n = 3;
+        int[][] edges = {{0, 1, 100}, {1, 2, 100}, {0, 2, 500}};
+        int src = 0, dst = 2, k = 1;
+        logResult(findCheapestPrice(n, edges, src, dst, k));
+    }
+
+    /**
+     * 787. K 站中转内最便宜的航班
+     *
+     * <p>有 n 个城市通过 m 个航班连接。每个航班都从城市 u 开始，以价格 w 抵达 v。
+     *
+     * <p>现在给定所有的城市和航班，以及出发城市 src 和目的地 dst，你的任务是找到从 src 到 dst 最多经过 k 站中转的最便宜的价格。 如果没有这样的路线，则输出 -1。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入: n = 3, edges = [[0,1,100],[1,2,100],[0,2,500]] src = 0, dst = 2, k = 1 输出: 200 解释:
+     * 城市航班图如下
+     *
+     * <p>从城市 0 到城市 2 在 1 站中转以内的最便宜价格是 200，如图中红色所示。 示例 2：
+     *
+     * <p>输入: n = 3, edges = [[0,1,100],[1,2,100],[0,2,500]] src = 0, dst = 2, k = 0 输出: 500 解释:
+     * 城市航班图如下
+     *
+     * <p>从城市 0 到城市 2 在 0 站中转以内的最便宜价格是 500，如图中蓝色所示。
+     *
+     * <p>提示：
+     *
+     * <p>n 范围是 [1, 100]，城市标签从 0 到 n - 1. 航班数量范围是 [0, n * (n - 1) / 2]. 每个航班的格式 (src, dst, price).
+     * 每个航班的价格范围是 [1, 10000]. k 范围是 [0, n - 1]. 航班没有重复，且不存在环路
+     *
+     * @param n
+     * @param flights
+     * @param src
+     * @param dst
+     * @param K
+     * @return
+     */
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
+        int result = Integer.MAX_VALUE;
+        int[][] graph = new int[n][n];
+        for (int[] flight : flights) {
+            graph[flight[0]][flight[1]] = flight[2];
+        }
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] {src, 0});
+        while (!queue.isEmpty()) {
+            if (K < 0) {
+                break;
+            }
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] cur = queue.poll();
+                int start = cur[0], price = cur[1];
+                for (int j = 0; j < n; j++) {
+                    if (graph[start][j] == 0) {
+                        continue;
+                    }
+                    int curPrice = price + graph[start][j];
+                    if (j == dst) {
+                        result = Math.min(result, curPrice);
+                    }
+                    queue.offer(new int[] {j, curPrice});
+                }
+            }
+            K--;
+        }
+
+        return result == Integer.MAX_VALUE ? -1 : result;
+    }
 }
