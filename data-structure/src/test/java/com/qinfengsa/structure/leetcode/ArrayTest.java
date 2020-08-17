a554f921630d1ee8578fc876b1827c58929ce07e
qinfengsa <15800012123@163.com>
2020-08-17 16:01:19 +0800

    2020-8-17 LeetCode刷题记录 1169. 查询无效交易 1171. 从链表中删去总和值为零的连续节点 1177. 构建回文串检测

--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/ArrayTest.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/ArrayTest.java
@@ -15134,4 +15134,83 @@ public class ArrayTest {
         */
         return (n - helf) * helf;
     }
+
+    /**
+     * 1169. 查询无效交易
+     *
+     * <p>如果出现下述两种情况，交易 可能无效：
+     *
+     * <p>交易金额超过 ¥1000 或者，它和另一个城市中同名的另一笔交易相隔不超过 60 分钟（包含 60 分钟整） 每个交易字符串 transactions[i]
+     * 由一些用逗号分隔的值组成，这些值分别表示交易的名称，时间（以分钟计），金额以及城市。
+     *
+     * <p>给你一份交易清单 transactions，返回可能无效的交易列表。你可以按任何顺序返回答案。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：transactions = ["alice,20,800,mtv","alice,50,100,beijing"]
+     * 输出：["alice,20,800,mtv","alice,50,100,beijing"] 解释：第一笔交易是无效的，因为第二笔交易和它间隔不超过 60
+     * 分钟、名称相同且发生在不同的城市。同样，第二笔交易也是无效的。 示例 2：
+     *
+     * <p>输入：transactions = ["alice,20,800,mtv","alice,50,1200,mtv"] 输出：["alice,50,1200,mtv"] 示例 3：
+     *
+     * <p>输入：transactions = ["alice,20,800,mtv","bob,50,1200,mtv"] 输出：["bob,50,1200,mtv"]
+     *
+     * <p>提示：
+     *
+     * <p>transactions.length <= 1000 每笔交易 transactions[i] 按 "{name},{time},{amount},{city}" 的格式进行记录
+     * 每个交易名称 {name} 和城市 {city} 都由小写英文字母组成，长度在 1 到 10 之间 每个交易时间 {time} 由一些数字组成，表示一个 0 到 1000 之间的整数
+     * 每笔交易金额 {amount} 由一些数字组成，表示一个 0 到 2000 之间的整数
+     *
+     * @param transactions
+     * @return
+     */
+    public List<String> invalidTransactions(String[] transactions) {
+        int len = transactions.length;
+        List<Transaction> list = new ArrayList<>();
+        List<String> result = new ArrayList<>();
+        for (String str : transactions) {
+            String[] strings = str.split(",");
+            list.add(
+                    new Transaction(
+                            strings[0],
+                            Integer.parseInt(strings[1]),
+                            Integer.parseInt(strings[2]),
+                            strings[3]));
+        }
+        for (int i = 0; i < len; i++) {
+            Transaction tran = list.get(i);
+            if (tran.amount > 1000) {
+                result.add(transactions[i]);
+                continue;
+            }
+            for (int j = 0; j < len; j++) {
+                if (i == j) {
+                    continue;
+                }
+                Transaction t2 = list.get(j);
+                if (Objects.equals(tran.name, t2.name)
+                        && !Objects.equals(tran.city, t2.city)
+                        && Math.abs(tran.time - t2.time) <= 60) {
+                    result.add(transactions[i]);
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    static class Transaction {
+        String name;
+        int time;
+        int amount;
+        String city;
+
+        public Transaction(String name, int minute, int amount, String city) {
+            this.name = name;
+            this.time = minute;
+            this.amount = amount;
+            this.city = city;
+        }
+    }
 }
--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/ListTest.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/ListTest.java
@@ -4,9 +4,11 @@ import static com.qinfengsa.structure.util.LogUtils.logResult;
 
 import java.util.ArrayList;
 import java.util.Deque;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
@@ -1735,5 +1737,53 @@ public class ListTest {
             }
         }
         return result;
+    }
+
+    /**
+     * 1171. 从链表中删去总和值为零的连续节点
+     *
+     * <p>给你一个链表的头节点 head，请你编写代码，反复删去链表中由 总和 值为 0 的连续节点组成的序列，直到不存在这样的序列为止。
+     *
+     * <p>删除完毕后，请你返回最终结果链表的头节点。
+     *
+     * <p>你可以返回任何满足题目要求的答案。
+     *
+     * <p>（注意，下面示例中的所有序列，都是对 ListNode 对象序列化的表示。）
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：head = [1,2,-3,3,1] 输出：[3,1] 提示：答案 [1,2,1] 也是正确的。 示例 2：
+     *
+     * <p>输入：head = [1,2,3,-3,4] 输出：[1,2,4] 示例 3：
+     *
+     * <p>输入：head = [1,2,3,-3,-2] 输出：[1]
+     *
+     * <p>提示：
+     *
+     * <p>给你的链表中可能有 1 到 1000 个节点。 对于链表中的每个节点，节点的值：-1000 <= node.val <= 1000.
+     *
+     * @param head
+     * @return
+     */
+    public ListNode removeZeroSumSublists(ListNode head) {
+        ListNode prev = new ListNode(0);
+        prev.next = head;
+        Map<Integer, ListNode> map = new HashMap<>();
+        int sum = 0;
+        // 前缀和
+        // 首次遍历建立 节点处链表和<->节点 哈希表
+        // 若同一和出现多次会覆盖，即记录该sum出现的最后一次节点
+        for (ListNode node = prev; Objects.nonNull(node); node = node.next) {
+            sum += node.val;
+            map.put(sum, node);
+        }
+
+        // 第二遍遍历 若当前节点处sum在下一处出现了则表明两结点之间所有节点和为0 直接删除区间所有节点
+        sum = 0;
+        for (ListNode node = prev; Objects.nonNull(node); node = node.next) {
+            sum += node.val;
+            node.next = map.get(sum).next;
+        }
+        return prev.next;
     }
 }
