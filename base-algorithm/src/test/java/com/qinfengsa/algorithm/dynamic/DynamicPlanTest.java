f0cfbac4757336f9a8bb865b3975b42d4793aa32
qinfengsa <15800012123@163.com>
2020-08-21 16:36:10 +0800

    2020-8-21 LeetCode刷题记录 1219. 黄金矿工 1222. 可以攻击国王的皇后 1262. 可被三整除的最大和

--- base-algorithm/src/test/java/com/qinfengsa/algorithm/dynamic/DynamicPlanTest.java
+++ base-algorithm/src/test/java/com/qinfengsa/algorithm/dynamic/DynamicPlanTest.java
@@ -5012,4 +5012,47 @@ public class DynamicPlanTest {
 
         return max;
     }
+
+    /**
+     * 1262. 可被三整除的最大和
+     *
+     * <p>给你一个整数数组 nums，请你找出并返回能被三整除的元素最大和。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：nums = [3,6,5,1,8] 输出：18 解释：选出数字 3, 6, 1 和 8，它们的和是 18（可被 3 整除的最大和）。 示例 2：
+     *
+     * <p>输入：nums = [4] 输出：0 解释：4 不能被 3 整除，所以无法选出数字，返回 0。 示例 3：
+     *
+     * <p>输入：nums = [1,2,3,4,4] 输出：12 解释：选出数字 1, 3, 4 以及 4，它们的和是 12（可被 3 整除的最大和）。
+     *
+     * <p>提示：
+     *
+     * <p>1 <= nums.length <= 4 * 10^4 1 <= nums[i] <= 10^4
+     *
+     * @param nums
+     * @return
+     */
+    public int maxSumDivThree(int[] nums) {
+        int len = nums.length;
+        // 余数0 余数1 余数2
+        int[] dp = new int[3];
+
+        dp[1] = Integer.MIN_VALUE;
+        dp[2] = Integer.MIN_VALUE;
+
+        for (int i = 0; i < len; i++) {
+            int mod = nums[i] % 3;
+            // 上一个 余数
+            int a = dp[(3 - mod) % 3];
+            int b = dp[(4 - mod) % 3];
+            int c = dp[(5 - mod) % 3];
+
+            dp[0] = Math.max(dp[0], a + nums[i]);
+            dp[1] = Math.max(dp[1], b + nums[i]);
+            dp[2] = Math.max(dp[2], c + nums[i]);
+        }
+
+        return dp[0];
+    }
 }
