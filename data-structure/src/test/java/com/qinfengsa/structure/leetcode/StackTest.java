1f54e4bd6885943eb229d9a6aea616aa4a65f8e9
qinfengsa <15800012123@163.com>
2020-08-25 11:37:22 +0800

    2020-8-25 LeetCode刷题记录 1234. 替换子串得到平衡字符串 1247. 交换字符使得字符串相同 1249. 移除无效的括号

--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/StackTest.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/StackTest.java
@@ -1065,4 +1065,75 @@ public class StackTest {
 
         return sb.toString();
     }
+
+    @Test
+    public void minRemoveToMakeValid() {
+        String s = "(a(b(c)d)";
+        logResult(minRemoveToMakeValid(s));
+    }
+
+    /**
+     * 1249. 移除无效的括号
+     *
+     * <p>给你一个由 '('、')' 和小写字母组成的字符串 s。
+     *
+     * <p>你需要从字符串中删除最少数目的 '(' 或者 ')' （可以删除任意位置的括号)，使得剩下的「括号字符串」有效。
+     *
+     * <p>请返回任意一个合法字符串。
+     *
+     * <p>有效「括号字符串」应当符合以下 任意一条 要求：
+     *
+     * <p>空字符串或只包含小写字母的字符串 可以被写作 AB（A 连接 B）的字符串，其中 A 和 B 都是有效「括号字符串」 可以被写作 (A) 的字符串，其中 A
+     * 是一个有效的「括号字符串」
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：s = "lee(t(c)o)de)" 输出："lee(t(c)o)de" 解释："lee(t(co)de)" , "lee(t(c)ode)" 也是一个可行答案。 示例
+     * 2：
+     *
+     * <p>输入：s = "a)b(c)d" 输出："ab(c)d" 示例 3：
+     *
+     * <p>输入：s = "))((" 输出："" 解释：空字符串也是有效的 示例 4：
+     *
+     * <p>输入：s = "(a(b(c)d)" 输出："a(b(c)d)"
+     *
+     * <p>提示：
+     *
+     * <p>1 <= s.length <= 10^5 s[i] 可能是 '('、')' 或英文小写字母
+     *
+     * @param s
+     * @return
+     */
+    public String minRemoveToMakeValid(String s) {
+        StringBuilder sb = new StringBuilder();
+        boolean[] invalid = new boolean[s.length()];
+        // 思路找到无效的括号,记录位置
+        Deque<Integer> stack = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                // 只记录左括号
+                stack.push(i);
+            }
+            if (stack.isEmpty() && c == ')') {
+                // 右括号无效
+                invalid[i] = true;
+            } else if (c == ')') {
+                stack.pop();
+            }
+        }
+        // 剩下的左括号无效
+        for (int index : stack) {
+            invalid[index] = true;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (invalid[i]) {
+                continue;
+            }
+            char c = s.charAt(i);
+            sb.append(c);
+        }
+        return sb.toString();
+    }
 }
--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/StringTest.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/StringTest.java
@@ -7893,4 +7893,62 @@ public class StringTest {
         }
         return sb.reverse().toString();
     }
+
+    /**
+     * 1234. 替换子串得到平衡字符串
+     *
+     * <p>有一个只含有 'Q', 'W', 'E', 'R' 四种字符，且长度为 n 的字符串。
+     *
+     * <p>假如在该字符串中，这四个字符都恰好出现 n/4 次，那么它就是一个「平衡字符串」。
+     *
+     * <p>给你一个这样的字符串 s，请通过「替换一个子串」的方式，使原字符串 s 变成一个「平衡字符串」。
+     *
+     * <p>你可以用和「待替换子串」长度相同的 任何 其他字符串来完成替换。
+     *
+     * <p>请返回待替换子串的最小可能长度。
+     *
+     * <p>如果原字符串自身就是一个平衡字符串，则返回 0。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：s = "QWER" 输出：0 解释：s 已经是平衡的了。 示例 2：
+     *
+     * <p>输入：s = "QQWE" 输出：1 解释：我们需要把一个 'Q' 替换成 'R'，这样得到的 "RQWE" (或 "QRWE") 是平衡的。 示例 3：
+     *
+     * <p>输入：s = "QQQW" 输出：2 解释：我们可以把前面的 "QQ" 替换成 "ER"。 示例 4：
+     *
+     * <p>输入：s = "QQQQ" 输出：3 解释：我们可以替换后 3 个 'Q'，使 s = "QWER"。
+     *
+     * <p>提示：
+     *
+     * <p>1 <= s.length <= 10^5 s.length 是 4 的倍数 s 中只含有 'Q', 'W', 'E', 'R' 四种字符
+     *
+     * @param s
+     * @return
+     */
+    public int balancedString(String s) {
+        int len = s.length();
+        int[] letters = new int[26];
+        for (char c : s.toCharArray()) {
+            letters[c - 'A']++;
+        }
+        int left = 0;
+        // 只需要考虑大于len/4 的字符
+        int quarterLen = len / 4;
+        int result = len;
+        for (int right = 0; right < len; right++) {
+            char c = s.charAt(right);
+            letters[c - 'A']--;
+            while (left < len
+                    && letters['Q' - 'A'] <= quarterLen
+                    && letters['W' - 'A'] <= quarterLen
+                    && letters['E' - 'A'] <= quarterLen
+                    && letters['R' - 'A'] <= quarterLen) {
+                result = Math.min(result, right - left + 1);
+                letters[s.charAt(left++) - 'A']++;
+            }
+        }
+
+        return result;
+    }
 }
