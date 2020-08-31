9e33f682deba150752ed5803b1d0c0be5ed63a6f
qinfengsa <15800012123@163.com>
2020-08-31 13:28:56 +0800

    2020-8-31 LeetCode刷题记录 1276. 不浪费原料的汉堡制作方案 1277. 统计全为 1 的正方形子矩阵 1282. 用户分组

--- base-algorithm/src/test/java/com/qinfengsa/algorithm/dynamic/DynamicPlanTest.java
+++ base-algorithm/src/test/java/com/qinfengsa/algorithm/dynamic/DynamicPlanTest.java
@@ -5293,4 +5293,41 @@ public class DynamicPlanTest {
 
         return max;
     }
+
+    /**
+     * 1277. 统计全为 1 的正方形子矩阵
+     *
+     * <p>给你一个 m * n 的矩阵，矩阵中的元素不是 0 就是 1，请你统计并返回其中完全由 1 组成的 正方形 子矩阵的个数。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：matrix = [ [0,1,1,1], [1,1,1,1], [0,1,1,1] ] 输出：15 解释： 边长为 1 的正方形有 10 个。 边长为 2 的正方形有 4
+     * 个。 边长为 3 的正方形有 1 个。 正方形的总数 = 10 + 4 + 1 = 15. 示例 2：
+     *
+     * <p>输入：matrix = [ [1,0,1], [1,1,0], [1,1,0] ] 输出：7 解释： 边长为 1 的正方形有 6 个。 边长为 2 的正方形有 1 个。
+     * 正方形的总数 = 6 + 1 = 7.
+     *
+     * <p>提示：
+     *
+     * <p>1 <= arr.length <= 300 1 <= arr[0].length <= 300 0 <= arr[i][j] <= 1
+     *
+     * @param matrix
+     * @return
+     */
+    public int countSquares(int[][] matrix) {
+        int rows = matrix.length, cols = matrix[0].length;
+        int result = 0;
+        int[][] dp = new int[rows + 1][cols + 1];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    continue;
+                }
+                dp[i + 1][j + 1] = Math.min(Math.min(dp[i][j + 1], dp[i + 1][j]), dp[i][j]) + 1;
+                result += dp[i + 1][j + 1];
+            }
+        }
+
+        return result;
+    }
 }
