32fd4b8ede5e733b4550f877dccc38a48ff71342
qinfengsa <15800012123@163.com>
2020-09-10 16:07:46 +0800

    2020-9-9 LeetCode刷题记录 1334. 阈值距离内邻居最少的城市 1338. 数组大小减半 1343. 大小为 K 且平均值大于等于阈值的子数组数目

--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/ArrayTest.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/ArrayTest.java
@@ -16928,4 +16928,109 @@ public class ArrayTest {
             return this.rating == that.rating ? that.id - this.id : that.rating - this.rating;
         }
     }
+
+    /**
+     * 1338. 数组大小减半
+     *
+     * <p>给你一个整数数组 arr。你可以从中选出一个整数集合，并删除这些整数在数组中的每次出现。
+     *
+     * <p>返回 至少 能删除数组中的一半整数的整数集合的最小大小。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：arr = [3,3,3,3,5,5,5,2,2,7] 输出：2 解释：选择 {3,7} 使得结果数组为 [5,5,5,2,2]、长度为 5（原数组长度的一半）。 大小为 2
+     * 的可行集合有 {3,5},{3,2},{5,2}。 选择 {2,7} 是不可行的，它的结果数组为 [3,3,3,3,5,5,5]，新数组长度大于原数组的二分之一。 示例 2：
+     *
+     * <p>输入：arr = [7,7,7,7,7,7] 输出：1 解释：我们只能选择集合 {7}，结果数组为空。 示例 3：
+     *
+     * <p>输入：arr = [1,9] 输出：1 示例 4：
+     *
+     * <p>输入：arr = [1000,1000,3,7] 输出：1 示例 5：
+     *
+     * <p>输入：arr = [1,2,3,4,5,6,7,8,9,10] 输出：5
+     *
+     * <p>提示：
+     *
+     * <p>1 <= arr.length <= 10^5 arr.length 为偶数 1 <= arr[i] <= 10^5
+     *
+     * @param arr
+     * @return
+     */
+    public int minSetSize(int[] arr) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int num : arr) {
+            int count = countMap.getOrDefault(num, 0);
+            countMap.put(num, count + 1);
+        }
+        PriorityQueue<Integer> heap = new PriorityQueue<>((a, b) -> b - a);
+        for (int count : countMap.values()) {
+            heap.offer(count);
+        }
+
+        /* List<Integer> list = new ArrayList<>(countMap.values());
+        list.sort((a, b) -> b - a);*/
+        int helf = arr.length >> 1;
+        if ((arr.length & 1) == 1) {
+            helf++;
+        }
+        int result = 0;
+        while (!heap.isEmpty()) {
+            helf -= heap.poll();
+            result++;
+            if (helf <= 0) {
+                break;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 1343. 大小为 K 且平均值大于等于阈值的子数组数目
+     *
+     * <p>给你一个整数数组 arr 和两个整数 k 和 threshold 。
+     *
+     * <p>请你返回长度为 k 且平均值大于等于 threshold 的子数组数目。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：arr = [2,2,2,2,5,5,5,8], k = 3, threshold = 4 输出：3 解释：子数组 [2,5,5],[5,5,5] 和 [5,5,8]
+     * 的平均值分别为 4，5 和 6 。其他长度为 3 的子数组的平均值都小于 4 （threshold 的值)。 示例 2：
+     *
+     * <p>输入：arr = [1,1,1,1,1], k = 1, threshold = 0 输出：5 示例 3：
+     *
+     * <p>输入：arr = [11,13,17,23,29,31,7,5,2,3], k = 3, threshold = 5 输出：6 解释：前 6 个长度为 3 的子数组平均值都大于 5
+     * 。注意平均值不是整数。 示例 4：
+     *
+     * <p>输入：arr = [7,7,7,7,7,7,7], k = 7, threshold = 7 输出：1 示例 5：
+     *
+     * <p>输入：arr = [4,4,4,4], k = 4, threshold = 1 输出：1
+     *
+     * <p>提示：
+     *
+     * <p>1 <= arr.length <= 10^5 1 <= arr[i] <= 10^4 1 <= k <= arr.length 0 <= threshold <= 10^4
+     *
+     * @param arr
+     * @param k
+     * @param threshold
+     * @return
+     */
+    public int numOfSubarrays(int[] arr, int k, int threshold) {
+        int target = k * threshold;
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+        int result = 0;
+        if (sum >= target) {
+            result++;
+        }
+        // 滑动窗口
+        for (int i = k; i < arr.length; i++) {
+            sum += arr[i] - arr[i - k];
+            if (sum >= target) {
+                result++;
+            }
+        }
+        return result;
+    }
 }
--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/GraphTest.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/GraphTest.java
@@ -1855,4 +1855,84 @@ public class GraphTest {
 
         return target;
     }
+
+    /**
+     * 1334. 阈值距离内邻居最少的城市
+     *
+     * <p>有 n 个城市，按从 0 到 n-1 编号。给你一个边数组 edges，其中 edges[i] = [fromi, toi, weighti] 代表 fromi 和 toi
+     * 两个城市之间的双向加权边，距离阈值是一个整数 distanceThreshold。
+     *
+     * <p>返回能通过某些路径到达其他城市数目最少、且路径距离 最大 为 distanceThreshold 的城市。如果有多个这样的城市，则返回编号最大的城市。
+     *
+     * <p>注意，连接城市 i 和 j 的路径的距离等于沿该路径的所有边的权重之和。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：n = 4, edges = [[0,1,3],[1,2,1],[1,3,4],[2,3,1]], distanceThreshold = 4 输出：3
+     * 解释：城市分布图如上。 每个城市阈值距离 distanceThreshold = 4 内的邻居城市分别是： 城市 0 -> [城市 1, 城市 2] 城市 1 -> [城市 0, 城市
+     * 2, 城市 3] 城市 2 -> [城市 0, 城市 1, 城市 3] 城市 3 -> [城市 1, 城市 2] 城市 0 和 3 在阈值距离 4 以内都有 2
+     * 个邻居城市，但是我们必须返回城市 3，因为它的编号最大。 示例 2：
+     *
+     * <p>输入：n = 5, edges = [[0,1,2],[0,4,8],[1,2,3],[1,4,2],[2,3,1],[3,4,1]], distanceThreshold = 2
+     * 输出：0 解释：城市分布图如上。 每个城市阈值距离 distanceThreshold = 2 内的邻居城市分别是： 城市 0 -> [城市 1] 城市 1 -> [城市 0, 城市
+     * 4] 城市 2 -> [城市 3, 城市 4] 城市 3 -> [城市 2, 城市 4] 城市 4 -> [城市 1, 城市 2, 城市 3] 城市 0 在阈值距离 4 以内只有 1
+     * 个邻居城市。
+     *
+     * <p>提示：
+     *
+     * <p>2 <= n <= 100 1 <= edges.length <= n * (n - 1) / 2 edges[i].length == 3 0 <= fromi < toi <
+     * n 1 <= weighti, distanceThreshold <= 10^4 所有 (fromi, toi) 都是不同的。
+     *
+     * @param n
+     * @param edges
+     * @param distanceThreshold
+     * @return
+     */
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        int[][] graph = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(graph[i], Integer.MAX_VALUE);
+        }
+
+        for (int[] edge : edges) {
+            graph[edge[0]][edge[1]] = edge[2];
+            graph[edge[1]][edge[0]] = edge[2];
+        }
+
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (i == j) {
+                        continue;
+                    }
+                    if (graph[i][k] == Integer.MAX_VALUE || graph[k][j] == Integer.MAX_VALUE) {
+                        continue;
+                    }
+                    graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+                }
+            }
+        }
+        int minCount = n;
+        int result = -1;
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (graph[i][j] <= distanceThreshold) {
+                    count++;
+                }
+            }
+            if (count < minCount) {
+                minCount = count;
+                result = i;
+            } else if (count == minCount) {
+                result = i;
+            }
+        }
+
+        return result;
+    }
 }
