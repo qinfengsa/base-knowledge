a6596b0ea7d0b9e59ba2a74bd32873b2a2b4059c
wangheng <15800012123@163.com>
2020-09-14 21:28:33 +0800

    2020-9-14 LeetCode刷题记录 LCP 17. 速算机器人 LCP 18. 早餐组合 LCP 19. 秋叶收藏集

--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/StringTest.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/StringTest.java
@@ -8455,4 +8455,77 @@ public class StringTest {
         String s = "abc";
         logResult(numberOfSubstrings(s));
     }
+
+    /**
+     * LCP 19. 秋叶收藏集
+     *
+     * <p>小扣出去秋游，途中收集了一些红叶和黄叶，他利用这些叶子初步整理了一份秋叶收藏集 leaves， 字符串 leaves 仅包含小写字符 r 和 y， 其中字符 r 表示一片红叶，字符
+     * y 表示一片黄叶。 出于美观整齐的考虑，小扣想要将收藏集中树叶的排列调整成「红、黄、红」三部分。每部分树叶数量可以不相等，但均需大于等于
+     * 1。每次调整操作，小扣可以将一片红叶替换成黄叶或者将一片黄叶替换成红叶。请问小扣最少需要多少次调整操作才能将秋叶收藏集调整完毕。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：leaves = "rrryyyrryyyrr"
+     *
+     * <p>输出：2
+     *
+     * <p>解释：调整两次，将中间的两片红叶替换成黄叶，得到 "rrryyyyyyyyrr"
+     *
+     * <p>示例 2：
+     *
+     * <p>输入：leaves = "ryr"
+     *
+     * <p>输出：0
+     *
+     * <p>解释：已符合要求，不需要额外操作
+     *
+     * <p>提示：
+     *
+     * <p>3 <= leaves.length <= 10^5 leaves 中只包含字符 'r' 和字符 'y'
+     *
+     * @param leaves
+     * @return
+     */
+    public int minimumOperations(String leaves) {
+        int len = leaves.length();
+        // 最终需要红黄红
+        // 维持3种状态，分别为截止目前全部红色，在截止目前全红的基础上变为黄色，以及变为黄色的基础上全部红色
+        int[][] dp = new int[len][3];
+        char[] chars = leaves.toCharArray();
+
+        dp[0][0] = chars[0] == 'r' ? 0 : 1;
+        for (int i = 1; i < len; i++) { // 设置第一个状态的值
+            if (chars[i] == 'r') {
+                dp[i][0] = dp[i - 1][0];
+            } else {
+                dp[i][0] = dp[i - 1][0] + 1;
+            }
+        }
+        if (chars[1] == 'y') {
+            dp[1][1] = dp[0][0]; // 设置第二个状态初值
+        } else {
+            dp[1][1] = dp[0][0] + 1;
+        }
+        for (int i = 2; i < len; i++) { // 设置第二个状态的值
+            if (chars[i] == 'r') {
+                dp[i][1] = Math.min(dp[i - 1][1], dp[i - 1][0]) + 1;
+            } else {
+                dp[i][1] = Math.min(dp[i - 1][1], dp[i - 1][0]);
+            }
+        }
+        if (chars[2] == 'r') {
+            dp[2][2] = dp[1][1]; // 设置第三个状态的值
+        } else {
+            dp[2][2] = dp[1][1] + 1;
+        }
+        for (int i = 3; i < len; i++) { // 设置第三个状态的初值
+            if (chars[i] == 'r') {
+                dp[i][2] = Math.min(dp[i - 1][1], dp[i - 1][2]);
+            } else {
+                dp[i][2] = Math.min(dp[i - 1][1], dp[i - 1][2]) + 1;
+            }
+        }
+
+        return dp[len - 1][2];
+    }
 }
