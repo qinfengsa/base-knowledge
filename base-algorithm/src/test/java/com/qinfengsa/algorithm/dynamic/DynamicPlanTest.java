63ae8c8a185b981ec6acf6a0ca8e4a84fa90084c
qinfengsa <15800012123@163.com>
2020-09-28 08:37:48 +0800

    2020-9-28 LeetCode刷题记录 789. 逃脱阻碍者 791. 自定义字符串排序 1043. 分隔数组以得到最大和

--- base-algorithm/src/test/java/com/qinfengsa/algorithm/dynamic/DynamicPlanTest.java
+++ base-algorithm/src/test/java/com/qinfengsa/algorithm/dynamic/DynamicPlanTest.java
@@ -5439,4 +5439,40 @@ public class DynamicPlanTest {
         logResult(dp);
         return dp[rows - 1][cols - 1][0] < 0 ? -1 : (int) dp[rows - 1][cols - 1][0] % MOD;
     }
+
+    /**
+     * 1043. 分隔数组以得到最大和
+     *
+     * <p>给出整数数组 A，将该数组分隔为长度最多为 K 的几个（连续）子数组。分隔完成后，每个子数组的中的值都会变为该子数组中的最大值。
+     *
+     * <p>返回给定数组完成分隔后的最大和。
+     *
+     * <p>示例：
+     *
+     * <p>输入：A = [1,15,7,9,2,5,10], K = 3 输出：84 解释：A 变为 [15,15,15,9,10,10,10]
+     *
+     * <p>提示：
+     *
+     * <p>1 <= K <= A.length <= 500 0 <= A[i] <= 10^6
+     *
+     * @param arr
+     * @param k
+     * @return
+     */
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        // 设dp[i]表示前i个元素能达到的最大和。考虑到长度最大为K，那么dp[i]可以由dp[i-K] ~ dp[i-1]推导而来，即状态转移为：
+        // dp[i] = max( dp[j] + max(A[j:i]) * (i - j) )，其中i - K <= j <= i - 1
+        int len = arr.length;
+        int[] dp = new int[len + 1];
+
+        for (int i = 1; i <= len; i++) {
+            int max = -1;
+            for (int j = i - 1; j >= Math.max(i - k, 0); j--) {
+                max = Math.max(max, arr[j]);
+                dp[i] = Math.max(dp[i], dp[j] + max * (i - j));
+            }
+        }
+
+        return dp[len];
+    }
 }
