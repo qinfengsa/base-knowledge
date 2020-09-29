0b66d0c6bf0cb31bf2f8892ababa98a351895df2
qinfengsa <15800012123@163.com>
2020-09-29 09:50:24 +0800

    2020-9-29 LeetCode刷题记录 792. 匹配子序列的单词数 794. 有效的井字游戏 795. 区间子数组个数

--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/ArrayTest.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/ArrayTest.java
@@ -18071,4 +18071,171 @@ public class ArrayTest {
 
         return false;
     }
+
+    @Test
+    public void validTicTacToe() {
+        String[] board = {"OXX", "XOX", "OXO"};
+        logResult(validTicTacToe(board));
+    }
+
+    /**
+     * 794. 有效的井字游戏
+     *
+     * <p>用字符串数组作为井字游戏的游戏板 board。当且仅当在井字游戏过程中，玩家有可能将字符放置成游戏板所显示的状态时，才返回 true。
+     *
+     * <p>该游戏板是一个 3 x 3 数组，由字符 " "，"X" 和 "O" 组成。字符 " " 代表一个空位。
+     *
+     * <p>以下是井字游戏的规则：
+     *
+     * <p>玩家轮流将字符放入空位（" "）中。 第一个玩家总是放字符 “X”，且第二个玩家总是放字符 “O”。 “X” 和 “O” 只允许放置在空位中，不允许对已放有字符的位置进行填充。
+     * 当有 3 个相同（且非空）的字符填充任何行、列或对角线时，游戏结束。 当所有位置非空时，也算为游戏结束。 如果游戏结束，玩家不允许再放置字符。 示例 1: 输入: board = ["O
+     * ", " ", " "] 输出: false 解释: 第一个玩家总是放置“X”。
+     *
+     * <p>示例 2: 输入: board = ["XOX", " X ", " "] 输出: false 解释: 玩家应该是轮流放置的。
+     *
+     * <p>示例 3: 输入: board = ["XXX", " ", "OOO"] 输出: false
+     *
+     * <p>示例 4: 输入: board = ["XOX", "O O", "XOX"] 输出: true 说明:
+     *
+     * <p>游戏板 board 是长度为 3 的字符串数组，其中每个字符串 board[i] 的长度为 3。 board[i][j] 是集合 {" ", "X", "O"} 中的一个字符。
+     *
+     * @param board
+     * @return
+     */
+    public boolean validTicTacToe(String[] board) {
+        int xCount = 0, oCount = 0;
+
+        for (String str : board) {
+            for (char c : str.toCharArray()) {
+                switch (c) {
+                    case 'X':
+                        xCount++;
+                        break;
+                    case 'O':
+                        oCount++;
+                        break;
+                }
+            }
+        }
+        if (xCount != oCount && (xCount - oCount) != 1) {
+            return false;
+        }
+        if (validTicTacToeWin(board, 'X') && (xCount - oCount) != 1) {
+            return false;
+        }
+        if (validTicTacToeWin(board, 'O') && xCount != oCount) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validTicTacToeWin(String[] board, char c) {
+        for (int i = 0; i < 3; i++) {
+            if (board[i].charAt(0) == c && board[i].charAt(1) == c && board[i].charAt(2) == c) {
+                return true;
+            }
+            if (board[0].charAt(i) == c && board[1].charAt(i) == c && board[2].charAt(i) == c) {
+                return true;
+            }
+        }
+        if (board[0].charAt(0) == c && board[1].charAt(1) == c && board[2].charAt(2) == c) {
+            return true;
+        }
+        if (board[0].charAt(2) == c && board[1].charAt(1) == c && board[2].charAt(0) == c) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * 795. 区间子数组个数
+     *
+     * <p>给定一个元素都是正整数的数组A ，正整数 L 以及 R (L <= R)。
+     *
+     * <p>求连续、非空且其中最大元素满足大于等于L 小于等于R的子数组个数。
+     *
+     * <p>例如 : 输入: A = [2, 1, 4, 3] L = 2 R = 3 输出: 3 解释: 满足条件的子数组: [2], [2, 1], [3]. 注意:
+     *
+     * <p>L, R 和 A[i] 都是整数，范围在 [0, 10^9]。 数组 A 的长度范围在[1, 50000]。
+     *
+     * @param A
+     * @param L
+     * @param R
+     * @return
+     */
+    public int numSubarrayBoundedMax(int[] A, int L, int R) {
+        // L ~ R 的 子数组个数 =  <= R的子数组个 - <=L-1 的子数组个数
+        return getSubarrayLessThen(A, R) - getSubarrayLessThen(A, L - 1);
+    }
+
+    /**
+     * <= target 的子数组个数
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    private int getSubarrayLessThen(int[] nums, int target) {
+        int result = 0, count = 0;
+        for (int num : nums) {
+            count = num <= target ? count + 1 : 0;
+            result += count;
+        }
+
+        return result;
+    }
+
+    /**
+     * 792. 匹配子序列的单词数
+     *
+     * <p>给定字符串 S 和单词字典 words, 求 words[i] 中是 S 的子序列的单词个数。
+     *
+     * <p>示例: 输入: S = "abcde" words = ["a", "bb", "acd", "ace"] 输出: 3 解释: 有三个是 S 的子序列的单词: "a",
+     * "acd", "ace"。 注意:
+     *
+     * <p>所有在words和 S 里的单词都只由小写字母组成。 S 的长度在 [1, 50000]。 words 的长度在 [1, 5000]。 words[i]的长度在[1, 50]。
+     *
+     * @param S
+     * @param words
+     * @return
+     */
+    public int numMatchingSubseq(String S, String[] words) {
+        Map<Character, List<WordNode>> letterMap = new HashMap<>();
+        for (String word : words) {
+            char c = word.charAt(0);
+            List<WordNode> list = letterMap.computeIfAbsent(c, k -> new ArrayList<>());
+            list.add(new WordNode(word));
+        }
+        int result = 0;
+        for (char c : S.toCharArray()) {
+            List<WordNode> list = letterMap.get(c);
+            if (Objects.isNull(list)) {
+                continue;
+            }
+            letterMap.remove(c);
+            for (WordNode node : list) {
+                node.index++;
+                if (node.index == node.word.length()) {
+                    result++;
+                } else {
+                    char next = node.word.charAt(node.index);
+                    List<WordNode> nextList =
+                            letterMap.computeIfAbsent(next, k -> new ArrayList<>());
+                    nextList.add(node);
+                }
+            }
+        }
+        return result;
+    }
+
+    static class WordNode {
+        String word;
+        int index;
+
+        WordNode(String word) {
+            this.word = word;
+            index = 0;
+        }
+    }
 }
