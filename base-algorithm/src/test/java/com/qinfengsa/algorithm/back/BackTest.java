57cb3433d792142b2a9a08cf019f7f9cb5135dee
qinfengsa <15800012123@163.com>
2020-09-30 10:16:40 +0800

    2020-9-30 LeetCode刷题记录 790. 多米诺和托米诺平铺 797. 所有可能的路径 799. 香槟塔

--- base-algorithm/src/test/java/com/qinfengsa/algorithm/back/BackTest.java
+++ base-algorithm/src/test/java/com/qinfengsa/algorithm/back/BackTest.java
@@ -2828,4 +2828,62 @@ public class BackTest {
             set.remove(str);
         }
     }
+
+    @Test
+    public void allPathsSourceTargetTest() {
+        int[][] graph = {{1, 2}, {3}, {3}, {}};
+        logResult(allPathsSourceTarget(graph));
+    }
+
+    List<List<Integer>> allPathsSourceTargetResult;
+    /**
+     * 797. 所有可能的路径
+     *
+     * <p>给一个有 n 个结点的有向无环图，找到所有从 0 到 n-1 的路径并输出（不要求按顺序）
+     *
+     * <p>二维数组的第 i 个数组中的单元都表示有向图中 i 号结点所能到达的下一些结点（译者注：有向图是有方向的，即规定了a→b你就不能从b→a）空就是没有下一个结点了。
+     *
+     * <p>示例: 输入: [[1,2], [3], [3], []] 输出: [[0,1,3],[0,2,3]] 解释: 图是这样的: 0--->1 | | v v 2--->3
+     * 这有两条路: 0 -> 1 -> 3 和 0 -> 2 -> 3. 提示:
+     *
+     * <p>结点的数量会在范围 [2, 15] 内。 你可以把路径以任意顺序输出，但在路径内的结点的顺序必须保证。
+     *
+     * @param graph
+     * @return
+     */
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        allPathsSourceTargetResult = new ArrayList<>();
+        int n = graph.length;
+        boolean[] visited = new boolean[n];
+        visited[0] = true;
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+        allPathsSourceTarget(0, n - 1, list, visited, graph);
+        return allPathsSourceTargetResult;
+    }
+
+    private void allPathsSourceTarget(
+            int start, int end, List<Integer> list, boolean[] visited, int[][] graph) {
+
+        if (start == end) {
+            allPathsSourceTargetResult.add(new ArrayList<>(list));
+            return;
+        }
+
+        int[] nextList = graph[start];
+        if (nextList.length == 0) {
+            return;
+        }
+        int size = list.size();
+        for (int next : nextList) {
+            if (visited[next]) {
+                continue;
+            }
+            visited[next] = true;
+            list.add(next);
+            allPathsSourceTarget(next, end, list, visited, graph);
+            visited[next] = false;
+            list.remove(size);
+        }
+    }
 }
--- base-algorithm/src/test/java/com/qinfengsa/algorithm/dynamic/DynamicPlanTest.java
+++ base-algorithm/src/test/java/com/qinfengsa/algorithm/dynamic/DynamicPlanTest.java
@@ -5475,4 +5475,85 @@ public class DynamicPlanTest {
 
         return dp[len];
     }
+
+    /**
+     * 790. 多米诺和托米诺平铺
+     *
+     * <p>有两种形状的瓷砖：一种是 2x1 的多米诺形，另一种是形如 "L" 的托米诺形。两种形状都可以旋转。
+     *
+     * <p>XX <- 多米诺
+     *
+     * <p>XX <- "L" 托米诺 X 给定 N 的值，有多少种方法可以平铺 2 x N 的面板？返回值 mod 10^9 + 7。
+     *
+     * <p>（平铺指的是每个正方形都必须有瓷砖覆盖。两个平铺不同，当且仅当面板上有四个方向上的相邻单元中的两个，使得恰好有一个平铺有一个瓷砖占据两个正方形。）
+     *
+     * <p>示例: 输入: 3 输出: 5 解释: 下面列出了五种不同的方法，不同字母代表不同瓷砖： XYZ XXZ XYY XXY XYY XYZ YYZ XZZ XYY XXY 提示：
+     *
+     * <p>N 的范围是 [1, 1000]
+     *
+     * @param N
+     * @return
+     */
+    public int numTilings(int N) {
+        // dp[n] = 2*dp[n-1] + dp[n-3]
+        int[] dp = new int[N + 3];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 5;
+        for (int i = 4; i <= N; i++) {
+            dp[i] = (2 * dp[i - 1] % MOD + dp[i - 3]) % MOD;
+        }
+        return dp[N];
+    }
+
+    @Test
+    public void champagneTower() {
+        int poured = 6, query_row = 3, query_glass = 1;
+        logResult(champagneTower(poured, query_row, query_glass));
+    }
+    /**
+     * 799. 香槟塔
+     *
+     * <p>我们把玻璃杯摆成金字塔的形状，其中第一层有1个玻璃杯，第二层有2个，依次类推到第100层，每个玻璃杯(250ml)将盛有香槟。
+     *
+     * <p>从顶层的第一个玻璃杯开始倾倒一些香槟，当顶层的杯子满了，任何溢出的香槟都会立刻等流量的流向左右两侧的玻璃杯。当左右两边的杯子也满了，就会等流量的流向它们左右两边的杯子，依次类推。（当最底层的玻璃杯满了，香槟会流到地板上）
+     *
+     * <p>例如，在倾倒一杯香槟后，最顶层的玻璃杯满了。倾倒了两杯香槟后，第二层的两个玻璃杯各自盛放一半的香槟。在倒三杯香槟后，第二层的香槟满了 -
+     * 此时总共有三个满的玻璃杯。在倒第四杯后，第三层中间的玻璃杯盛放了一半的香槟，他两边的玻璃杯各自盛放了四分之一的香槟，如下图所示。
+     *
+     * <p>现在当倾倒了非负整数杯香槟后，返回第 i 行 j 个玻璃杯所盛放的香槟占玻璃杯容积的比例（i 和 j都从0开始）。
+     *
+     * <p>示例 1: 输入: poured(倾倒香槟总杯数) = 1, query_glass(杯子的位置数) = 1, query_row(行数) = 1 输出: 0.0 解释:
+     * 我们在顶层（下标是（0，0））倒了一杯香槟后，没有溢出，因此所有在顶层以下的玻璃杯都是空的。
+     *
+     * <p>示例 2: 输入: poured(倾倒香槟总杯数) = 2, query_glass(杯子的位置数) = 1, query_row(行数) = 1 输出: 0.5 解释:
+     * 我们在顶层（下标是（0，0）倒了两杯香槟后，有一杯量的香槟将从顶层溢出，位于（1，0）的玻璃杯和（1，1）的玻璃杯平分了这一杯香槟，所以每个玻璃杯有一半的香槟。 注意:
+     *
+     * <p>poured 的范围[0, 10 ^ 9]。 query_glass 和query_row 的范围 [0, 99]。
+     *
+     * @param poured
+     * @param query_row
+     * @param query_glass
+     * @return
+     */
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        double[][] dp = new double[query_row + 1][query_row + 1];
+        dp[0][0] = poured;
+        for (int i = 1; i <= query_row; i++) {
+            for (int j = 0; j <= i; j++) {
+                double left = j == 0 ? 0.0 : dp[i - 1][j - 1];
+                double right = j == i ? 0.0 : dp[i - 1][j];
+
+                left = left <= 1.0 ? 0 : left - 1.0;
+                right = right <= 1.0 ? 0 : right - 1.0;
+                if (left + right <= 0.0) {
+                    continue;
+                }
+                dp[i][j] = (left + right) / 2.0;
+            }
+        }
+        logResult(dp);
+        return Math.min(dp[query_row][query_glass], 1.0);
+    }
 }
