7a6f765d56633a0f2e76bf26cf751de6cd519da4
qinfengsa <15800012123@163.com>
2020-10-01 21:21:37 +0800

    2020-10-1 LeetCode刷题记录 801. 使序列递增的最小交换次数 807. 保持城市天际线 814. 二叉树剪枝

--- base-algorithm/src/test/java/com/qinfengsa/algorithm/dynamic/DynamicPlanTest.java
+++ base-algorithm/src/test/java/com/qinfengsa/algorithm/dynamic/DynamicPlanTest.java
@@ -5556,4 +5556,44 @@ public class DynamicPlanTest {
         logResult(dp);
         return Math.min(dp[query_row][query_glass], 1.0);
     }
+
+    /**
+     * 801. 使序列递增的最小交换次数
+     *
+     * <p>我们有两个长度相等且不为空的整型数组 A 和 B 。
+     *
+     * <p>我们可以交换 A[i] 和 B[i] 的元素。注意这两个元素在各自的序列中应该处于相同的位置。
+     *
+     * <p>在交换过一些元素之后，数组 A 和 B 都应该是严格递增的（数组严格递增的条件仅为A[0] < A[1] < A[2] < ... < A[A.length - 1]）。
+     *
+     * <p>给定数组 A 和 B ，请返回使得两个数组均保持严格递增状态的最小交换次数。假设给定的输入总是有效的。
+     *
+     * <p>示例: 输入: A = [1,3,5,4], B = [1,2,3,7] 输出: 1 解释: 交换 A[3] 和 B[3] 后，两个数组如下: A = [1, 3, 5, 7] ，
+     * B = [1, 2, 3, 4] 两个数组均为严格递增的。 注意:
+     *
+     * <p>A, B 两个数组的长度总是相等的，且长度的范围为 [1, 1000]。 A[i], B[i] 均为 [0, 2000]区间内的整数。
+     *
+     * @param A
+     * @param B
+     * @return
+     */
+    public int minSwap(int[] A, int[] B) {
+        // n 不交换  s 交换
+        int n = 0, s = 1;
+        for (int i = 1; i < A.length; i++) {
+            int n2 = Integer.MAX_VALUE, s2 = Integer.MAX_VALUE;
+            if (A[i - 1] < A[i] && B[i - 1] < B[i]) {
+                n2 = Math.min(n2, n);
+                s2 = Math.min(s2, s + 1);
+            }
+            if (A[i - 1] < B[i] && B[i - 1] < A[i]) {
+                n2 = Math.min(n2, s);
+                s2 = Math.min(s2, n + 1);
+            }
+            n = n2;
+            s = s2;
+        }
+
+        return Math.min(n, s);
+    }
 }
--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/TreeTest.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/TreeTest.java
@@ -5378,4 +5378,48 @@ public class TreeTest {
         list.add(root);
         inOrder2(root.right, list);
     }
+
+    /**
+     * 814. 二叉树剪枝
+     *
+     * <p>给定二叉树根结点 root ，此外树的每个结点的值要么是 0，要么是 1。
+     *
+     * <p>返回移除了所有不包含 1 的子树的原二叉树。
+     *
+     * <p>( 节点 X 的子树为 X 本身，以及所有 X 的后代。)
+     *
+     * <p>示例1: 输入: [1,null,0,0,1] 输出: [1,null,0,null,1]
+     *
+     * <p>解释: 只有红色节点满足条件“所有不包含 1 的子树”。 右图为返回的答案。
+     *
+     * <p>示例2: 输入: [1,0,1,0,0,0,1] 输出: [1,null,1,null,1]
+     *
+     * <p>示例3: 输入: [1,1,0,1,1,0,1,0] 输出: [1,1,0,1,1,null,1]
+     *
+     * <p>说明:
+     *
+     * <p>给定的二叉树最多有 100 个节点。 每个节点的值只会为 0 或 1 。
+     *
+     * @param root
+     * @return
+     */
+    public TreeNode pruneTree(TreeNode root) {
+
+        return containsOne(root) ? root : null;
+    }
+
+    private boolean containsOne(TreeNode root) {
+        if (Objects.isNull(root)) {
+            return false;
+        }
+        boolean left = containsOne(root.left);
+        boolean right = containsOne(root.right);
+        if (!left) {
+            root.left = null;
+        }
+        if (!right) {
+            root.right = null;
+        }
+        return root.val == 1 || left || right;
+    }
 }
