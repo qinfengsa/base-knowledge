329f123a456de09a5b01905d4dd57c0489fb8aad
qinfengsa <15800012123@163.com>
2020-10-02 20:09:39 +0800

    2020-10-2 LeetCode刷题记录 817. 链表组件 823. 带因子的二叉树 845. 数组中的最长山脉

--- base-algorithm/src/test/java/com/qinfengsa/algorithm/dynamic/DynamicPlanTest.java
+++ base-algorithm/src/test/java/com/qinfengsa/algorithm/dynamic/DynamicPlanTest.java
@@ -5596,4 +5596,59 @@ public class DynamicPlanTest {
 
         return Math.min(n, s);
     }
+
+    /**
+     * 823. 带因子的二叉树
+     *
+     * <p>给出一个含有不重复整数元素的数组，每个整数均大于 1。
+     *
+     * <p>我们用这些整数来构建二叉树，每个整数可以使用任意次数。
+     *
+     * <p>其中：每个非叶结点的值应等于它的两个子结点的值的乘积。
+     *
+     * <p>满足条件的二叉树一共有多少个？返回的结果应模除 10 ** 9 + 7。
+     *
+     * <p>示例 1:
+     *
+     * <p>输入: A = [2, 4] 输出: 3 解释: 我们可以得到这些二叉树: [2], [4], [4, 2, 2] 示例 2:
+     *
+     * <p>输入: A = [2, 4, 5, 10] 输出: 7 解释: 我们可以得到这些二叉树: [2], [4], [5], [10], [4, 2, 2], [10, 2, 5],
+     * [10, 5, 2].
+     *
+     * <p>提示:
+     *
+     * <p>1 <= A.length <= 1000. 2 <= A[i] <= 10 ^ 9.
+     *
+     * @param A
+     * @return
+     */
+    public int numFactoredBinaryTrees(int[] A) {
+        Arrays.sort(A);
+        int len = A.length;
+        long[] dp = new long[len];
+        Arrays.fill(dp, 1);
+        Map<Integer, Integer> indexMap = new HashMap<>();
+        for (int i = 0; i < len; i++) {
+            indexMap.put(A[i], i);
+        }
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < i; j++) {
+                if (A[i] % A[j] != 0) {
+                    continue;
+                }
+                int l = A[i] / A[j];
+                if (!indexMap.containsKey(l)) {
+                    continue;
+                }
+                dp[i] += dp[j] * dp[indexMap.get(l)] % MOD;
+                dp[i] %= MOD;
+            }
+        }
+        long result = 0;
+        for (long num : dp) {
+            result += num;
+            result %= MOD;
+        }
+        return (int) result;
+    }
 }
--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/ListTest.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/ListTest.java
@@ -1786,4 +1786,47 @@ public class ListTest {
         }
         return prev.next;
     }
+
+    /**
+     * 817. 链表组件
+     *
+     * <p>给定链表头结点 head，该链表上的每个结点都有一个 唯一的整型值 。
+     *
+     * <p>同时给定列表 G，该列表是上述链表中整型值的一个子集。
+     *
+     * <p>返回列表 G 中组件的个数，这里对组件的定义为：链表中一段最长连续结点的值（该值必须在列表 G 中）构成的集合。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入: head: 0->1->2->3 G = [0, 1, 3] 输出: 2 解释: 链表中,0 和 1 是相连接的，且 G 中不包含 2，所以 [0, 1] 是 G
+     * 的一个组件，同理 [3] 也是一个组件，故返回 2。 示例 2：
+     *
+     * <p>输入: head: 0->1->2->3->4 G = [0, 3, 1, 4] 输出: 2 解释: 链表中，0 和 1 是相连接的，3 和 4 是相连接的，所以 [0, 1] 和
+     * [3, 4] 是两个组件，故返回 2。
+     *
+     * <p>提示：
+     *
+     * <p>如果 N 是给定链表 head 的长度，1 <= N <= 10000。 链表中每个结点的值所在范围为 [0, N - 1]。 1 <= G.length <= 10000 G
+     * 是链表中所有结点的值的一个子集.
+     *
+     * @param head
+     * @param G
+     * @return
+     */
+    public int numComponents(ListNode head, int[] G) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : G) {
+            set.add(num);
+        }
+        ListNode node = head;
+        int result = 0;
+        while (Objects.nonNull(node)) {
+            if (set.contains(node.val) && (node.next == null || !set.contains(node.next.val))) {
+                result++;
+            }
+            node = node.next;
+        }
+
+        return result;
+    }
 }
