d8fb40315ae1868f196282e29a9cd5d2ed8ab469
qinfengsa <15800012123@163.com>
2020-10-03 23:40:34 +0800

    2020-10-3 LeetCode刷题记录 5515. 设计停车系统 5516. 警告一小时内使用相同员工卡大于等于三次的人 5518. 给定行和列的和求可行矩阵

--- base-algorithm/src/test/java/com/qinfengsa/algorithm/greedy/GreedyTest.java
+++ base-algorithm/src/test/java/com/qinfengsa/algorithm/greedy/GreedyTest.java
@@ -1568,4 +1568,68 @@ public class GreedyTest {
         }
         return profit < 0 ? -1 : max;
     }
+
+    @Test
+    public void restoreMatrix() {
+        int[] rowSum = {0}, colSum = {0};
+        int[][] result = restoreMatrix(rowSum, colSum);
+        logResult(result);
+    }
+    /**
+     * 5518. 给定行和列的和求可行矩阵
+     *
+     * <p>给你两个非负整数数组 rowSum 和 colSum ，其中 rowSum[i] 是二维矩阵中第 i 行元素的和， colSum[j] 是第 j
+     * 列元素的和。换言之你不知道矩阵里的每个元素，但是你知道每一行和每一列的和。
+     *
+     * <p>请找到大小为 rowSum.length x colSum.length 的任意 非负整数 矩阵，且该矩阵满足 rowSum 和 colSum 的要求。
+     *
+     * <p>请你返回任意一个满足题目要求的二维矩阵，题目保证存在 至少一个 可行矩阵。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：rowSum = [3,8], colSum = [4,7] 输出：[[3,0], [1,7]] 解释： 第 0 行：3 + 0 = 0 == rowSum[0] 第 1
+     * 行：1 + 7 = 8 == rowSum[1] 第 0 列：3 + 1 = 4 == colSum[0] 第 1 列：0 + 7 = 7 == colSum[1]
+     * 行和列的和都满足题目要求，且所有矩阵元素都是非负的。 另一个可行的矩阵为：[[1,2], [3,5]] 示例 2：
+     *
+     * <p>输入：rowSum = [5,7,10], colSum = [8,6,8] 输出：[[0,5,0], [6,1,0], [2,0,8]] 示例 3：
+     *
+     * <p>输入：rowSum = [14,9], colSum = [6,9,8] 输出：[[0,9,5], [6,0,3]] 示例 4：
+     *
+     * <p>输入：rowSum = [1,0], colSum = [1] 输出：[[1], [0]] 示例 5：
+     *
+     * <p>输入：rowSum = [0], colSum = [0] 输出：[[0]]
+     *
+     * <p>提示：
+     *
+     * <p>1 <= rowSum.length, colSum.length <= 500 0 <= rowSum[i], colSum[i] <= 108 sum(rows) ==
+     * sum(columns)
+     *
+     * @param rowSum
+     * @param colSum
+     * @return
+     */
+    public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
+        int rows = rowSum.length, cols = colSum.length;
+        int[][] result = new int[rows][cols];
+        int len = Math.min(rows, cols);
+        for (int i = 0; i < len; i++) {
+            int num = Math.min(rowSum[i], colSum[i]);
+            result[i][i] = num;
+            rowSum[i] -= num;
+            colSum[i] -= num;
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (rowSum[i] == 0 || colSum[j] == 0) {
+                    continue;
+                }
+                int num = Math.min(rowSum[i], colSum[j]);
+                result[i][j] = num;
+                rowSum[i] -= num;
+                colSum[j] -= num;
+            }
+        }
+
+        return result;
+    }
 }
--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/ArrayTest.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/ArrayTest.java
@@ -17,6 +17,7 @@ import java.util.Objects;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Set;
+import java.util.SortedSet;
 import java.util.Stack;
 import java.util.TreeMap;
 import java.util.TreeSet;
@@ -18331,5 +18332,98 @@ public class ArrayTest {
         }
 
         return result;
+    }
+
+    @Test
+    public void alertNames() {
+        String[] keyName = {"a", "a", "a", "a", "a", "a", "b", "b", "b", "b", "b"},
+                keyTime =
+                        {
+                            "23:27", "03:14", "12:57", "13:35", "13:18", "21:58", "22:39", "10:49",
+                            "19:37", "14:14", "10:41"
+                        };
+
+        logResult(alertNames(keyName, keyTime));
+    }
+    /**
+     * 5516. 警告一小时内使用相同员工卡大于等于三次的人
+     *
+     * <p>力扣公司的员工都使用员工卡来开办公室的门。每当一个员工使用一次他的员工卡，安保系统会记录下员工的名字和使用时间。如果一个员工在一小时时间内使用员工卡的次数大于等于三次，这个系统会自动发布一个
+     * 警告 。
+     *
+     * <p>给你字符串数组 keyName 和 keyTime ，期中 [keyName[i], keyTime[i]] 对应一个人的名字和他在 某一天 内使用员工卡的时间。
+     *
+     * <p>使用时间的格式是 24小时制 ，形如 "HH:MM" ，比方说 "23:51" 和 "09:49" 。
+     *
+     * <p>请你返回去重后的收到系统警告的员工名字，将它们按 字典序升序 排序后返回。
+     *
+     * <p>请注意 "10:00" - "11:00" 视为一个小时时间范围内，而 "23:51" - "00:10" 不被视为一小时内，因为系统记录的是某一天内的使用情况。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：keyName = ["daniel","daniel","daniel","luis","luis","luis","luis"], keyTime =
+     * ["10:00","10:40","11:00","09:00","11:00","13:00","15:00"] 输出：["daniel"] 解释："daniel" 在一小时内使用了
+     * 3 次员工卡（"10:00"，"10:40"，"11:00"）。 示例 2：
+     *
+     * <p>输入：keyName = ["alice","alice","alice","bob","bob","bob","bob"], keyTime =
+     * ["12:01","12:00","18:00","21:00","21:20","21:30","23:00"] 输出：["bob"] 解释："bob" 在一小时内使用了 3
+     * 次员工卡（"21:00"，"21:20"，"21:30"）。 示例 3：
+     *
+     * <p>输入：keyName = ["john","john","john"], keyTime = ["23:58","23:59","00:01"] 输出：[] 示例 4：
+     *
+     * <p>输入：keyName = ["leslie","leslie","leslie","clare","clare","clare","clare"], keyTime =
+     * ["13:00","13:20","14:00","18:00","18:51","19:30","19:49"] 输出：["clare","leslie"]
+     *
+     * <p>提示：
+     *
+     * <p>1 <= keyName.length, keyTime.length <= 105 keyName.length == keyTime.length keyTime 格式为
+     * "HH:MM" 。 保证 [keyName[i], keyTime[i]] 形成的二元对 互不相同 。 1 <= keyName[i].length <= 10 keyName[i]
+     * 只包含小写英文字母。
+     *
+     * @param keyName
+     * @param keyTime
+     * @return
+     */
+    public List<String> alertNames(String[] keyName, String[] keyTime) {
+
+        Set<String> set = new HashSet<>();
+        Map<String, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < keyName.length; i++) {
+            String name = keyName[i];
+            int time = getTime(keyTime[i]);
+            List<Integer> list = map.computeIfAbsent(name, k -> new ArrayList<>());
+            list.add(time);
+        }
+        for (Map.Entry<String, List<Integer>> entry : map.entrySet()) {
+            String name = entry.getKey();
+            List<Integer> list = entry.getValue();
+            Collections.sort(list);
+            TreeSet<Integer> treeSet = new TreeSet<>();
+            out:
+            for (int time : list) {
+                treeSet.add(time);
+                SortedSet<Integer> count = treeSet.subSet(time - 60, time + 1);
+                if (count.size() >= 3) {
+                    set.add(name);
+                    break out;
+                }
+            }
+        }
+
+        List<String> result = new ArrayList<>(set);
+        Collections.sort(result);
+        return result;
+    }
+
+    private int getTime(String s) {
+        int hours = Integer.valueOf(s.substring(0, 2));
+        int mins = Integer.valueOf(s.substring(3));
+        return hours * 60 + mins;
+    }
+
+    @Test
+    public void test11() {
+        String s = "10:30";
+        logResult(getTime(s));
     }
 }
