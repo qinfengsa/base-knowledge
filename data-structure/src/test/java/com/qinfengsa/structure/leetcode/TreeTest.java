9ccd6825f3aa396f8c174f5e8ca2c1c03780cbfa
qinfengsa <15800012123@163.com>
2020-10-12 12:09:38 +0800

    2020-10-12 LeetCode刷题记录 530. 二叉搜索树的最小绝对差 835. 图像重叠 838. 推多米诺 842. 将数组拆分成斐波那契序列

--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/TreeTest.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/TreeTest.java
@@ -5608,4 +5608,48 @@ public class TreeTest {
             preOrder(neighbor, root);
         }
     }
+
+    /**
+     * 530. 二叉搜索树的最小绝对差
+     *
+     * <p>给你一棵所有节点为非负值的二叉搜索树，请你计算树中任意两节点的差的绝对值的最小值。
+     *
+     * <p>示例：
+     *
+     * <p>输入：
+     *
+     * <p>1 \ 3 / 2
+     *
+     * <p>输出： 1
+     *
+     * <p>解释： 最小绝对差为 1，其中 2 和 1 的差的绝对值为 1（或者 2 和 3）。
+     *
+     * <p>提示：
+     *
+     * <p>树中至少有 2 个节点。 本题与 783 https://leetcode-cn.com/problems/minimum-distance-between-bst-nodes/
+     * 相同
+     *
+     * @param root
+     * @return
+     */
+    public int getMinimumDifference(TreeNode root) {
+        minDiff = Integer.MAX_VALUE;
+        inOrder(root);
+        return minDiff;
+    }
+
+    int preVal = -1;
+
+    private void inOrder(TreeNode root) {
+        if (Objects.isNull(root)) {
+            return;
+        }
+        inOrder(root.left);
+        if (preVal != -1) {
+            minDiff = Math.min(minDiff, root.val - preVal);
+        }
+
+        preVal = root.val;
+        inOrder(root.right);
+    }
 }
