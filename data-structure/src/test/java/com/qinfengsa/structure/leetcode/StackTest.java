6bc5f4f8409651d5d61fd8512b95902ef1d8d8ed
qinfengsa <15800012123@163.com>
2020-10-14 10:12:21 +0800

    2020-10-14 LeetCode刷题记录 853. 车队 856. 括号的分数 858. 镜面反射

--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/StackTest.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/StackTest.java
@@ -1188,4 +1188,44 @@ public class StackTest {
         }
         return max;
     }
+
+    /**
+     * 856. 括号的分数
+     *
+     * <p>给定一个平衡括号字符串 S，按下述规则计算该字符串的分数：
+     *
+     * <p>() 得 1 分。 AB 得 A + B 分，其中 A 和 B 是平衡括号字符串。 (A) 得 2 * A 分，其中 A 是平衡括号字符串。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入： "()" 输出： 1 示例 2：
+     *
+     * <p>输入： "(())" 输出： 2 示例 3：
+     *
+     * <p>输入： "()()" 输出： 2 示例 4：
+     *
+     * <p>输入： "(()(()))" 输出： 6
+     *
+     * <p>提示：
+     *
+     * <p>S 是平衡括号字符串，且只含有 ( 和 ) 。 2 <= S.length <= 50
+     *
+     * @param S
+     * @return
+     */
+    public int scoreOfParentheses(String S) {
+        Deque<Integer> stack = new LinkedList<>();
+        stack.push(0);
+        for (char c : S.toCharArray()) {
+            if (c == '(') {
+                stack.push(0);
+            } else {
+                int v = stack.pop();
+                int w = stack.pop();
+                stack.push(w + Math.max(2 * v, 1));
+            }
+        }
+
+        return stack.pop();
+    }
 }
