bbc48234b725b25c015fff07b3bb9e253a488682
qinfengsa <15800012123@163.com>
2020-10-25 12:57:40 +0800

    2020-10-25 LeetCode刷题记录 5546. 按键持续时间最长的键 5547. 等差子数组 5548. 最小体力消耗路径

--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/ArrayTest.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/ArrayTest.java
@@ -19207,4 +19207,156 @@ public class ArrayTest {
             }
         }
     }
+
+    /**
+     * 5547. 等差子数组
+     *
+     * <p>如果一个数列由至少两个元素组成，且每两个连续元素之间的差值都相同，那么这个序列就是 等差数列 。更正式地，数列 s 是等差数列，只需要满足：对于每个有效的 i ， s[i+1] -
+     * s[i] == s[1] - s[0] 都成立。
+     *
+     * <p>例如，下面这些都是 等差数列 ：
+     *
+     * <p>1, 3, 5, 7, 9 7, 7, 7, 7 3, -1, -5, -9 下面的数列 不是等差数列 ：
+     *
+     * <p>1, 1, 2, 5, 7 给你一个由 n 个整数组成的数组 nums，和两个由 m 个整数组成的数组 l 和 r，后两个数组表示 m 组范围查询，其中第 i 个查询对应范围
+     * [l[i], r[i]] 。所有数组的下标都是 从 0 开始 的。
+     *
+     * <p>返回 boolean 元素构成的答案列表 answer 。如果子数组 nums[l[i]], nums[l[i]+1], ... , nums[r[i]] 可以 重新排列 形成
+     * 等差数列 ，answer[i] 的值就是 true；否则answer[i] 的值就是 false 。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：nums = [4,6,5,9,3,7], l = [0,0,2], r = [2,3,5] 输出：[true,false,true] 解释： 第 0 个查询，对应子数组
+     * [4,6,5] 。可以重新排列为等差数列 [6,5,4] 。 第 1 个查询，对应子数组 [4,6,5,9] 。无法重新排列形成等差数列。 第 2 个查询，对应子数组 [5,9,3,7]
+     * 。可以重新排列为等差数列 [3,5,7,9] 。 示例 2：
+     *
+     * <p>输入：nums = [-12,-9,-3,-12,-6,15,20,-25,-20,-15,-10], l = [0,1,6,4,8,7], r = [4,4,9,7,9,10]
+     * 输出：[false,true,false,false,true,true]
+     *
+     * <p>提示：
+     *
+     * <p>n == nums.length m == l.length m == r.length 2 <= n <= 500 1 <= m <= 500 0 <= l[i] < r[i]
+     * < n -105 <= nums[i] <= 105
+     *
+     * @param nums
+     * @param l
+     * @param r
+     * @return
+     */
+    public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
+        List<Boolean> result = new ArrayList<>();
+        int len = l.length;
+        for (int i = 0; i < len; i++) {
+            int[] arr = Arrays.copyOfRange(nums, l[i], r[i] + 1);
+            log.debug("arr:{}", arr);
+            Arrays.sort(arr);
+            boolean b = canMakeArithmeticProgression(arr);
+            result.add(b);
+        }
+
+        return result;
+    }
+
+    @Test
+    public void checkArithmeticSubarrays() {
+        int[] nums = {-12, -9, -3, -12, -6, 15, 20, -25, -20, -15, -10},
+                l = {0, 1, 6, 4, 8, 7},
+                r = {4, 4, 9, 7, 9, 10};
+        logResult(checkArithmeticSubarrays(nums, l, r));
+    }
+
+    /**
+     * 5548. 最小体力消耗路径
+     *
+     * <p>你准备参加一场远足活动。给你一个二维 rows x columns 的地图 heights ，其中 heights[row][col] 表示格子 (row, col)
+     * 的高度。一开始你在最左上角的格子 (0, 0) ，且你希望去最右下角的格子 (rows-1, columns-1) （注意下标从 0 开始编号）。你每次可以往 上，下，左，右
+     * 四个方向之一移动，你想要找到耗费 体力 最小的一条路径。
+     *
+     * <p>一条路径耗费的 体力值 是路径上相邻格子之间 高度差绝对值 的 最大值 决定的。
+     *
+     * <p>请你返回从左上角走到右下角的最小 体力消耗值 。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：heights = [[1,2,2],[3,8,2],[5,3,5]] 输出：2 解释：路径 [1,3,5,3,5] 连续格子的差值绝对值最大为 2 。 这条路径比路径
+     * [1,2,2,2,5] 更优，因为另一条路劲差值最大值为 3 。 示例 2：
+     *
+     * <p>输入：heights = [[1,2,3],[3,8,4],[5,3,5]] 输出：1 解释：路径 [1,2,3,4,5] 的相邻格子差值绝对值最大为 1 ，比路径
+     * [1,3,5,3,5] 更优。 示例 3：
+     *
+     * <p>输入：heights = [[1,2,1,1,1],[1,2,1,2,1],[1,2,1,2,1],[1,2,1,2,1],[1,1,1,2,1]] 输出：0
+     * 解释：上图所示路径不需要消耗任何体力。
+     *
+     * <p>提示：
+     *
+     * <p>rows == heights.length columns == heights[i].length 1 <= rows, columns <= 100 1 <=
+     * heights[i][j] <= 106
+     *
+     * @param heights
+     * @return
+     */
+    public int minimumEffortPath(int[][] heights) {
+        // 二分
+        grid = heights;
+        M = heights.length;
+        N = heights[0].length;
+        int left = 0, right = 0;
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+                if (i > 0) {
+                    right = Math.max(right, Math.abs(heights[i][j] - heights[i - 1][j]));
+                }
+                if (j > 0) {
+                    right = Math.max(right, Math.abs(heights[i][j] - heights[i][j - 1]));
+                }
+            }
+        }
+
+        while (left < right) {
+            int mid = (left + right) >> 1;
+            if (dfsEffortPath(0, 0, mid, new boolean[M][N])) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    /**
+     * 能否以 最大max 到达 终点
+     *
+     * @param row
+     * @param col
+     * @param max
+     * @param visited
+     * @return
+     */
+    private boolean dfsEffortPath(int row, int col, int max, boolean[][] visited) {
+        if (row == M - 1 && col == N - 1) {
+            return true;
+        }
+        visited[row][col] = true;
+        for (int i = 0; i < 4; i++) {
+            int rowNum = row + DIR_ROW[i], colNum = col + DIR_COL[i];
+            if (!inArea(rowNum, colNum, M, N)) {
+                continue;
+            }
+            if (visited[rowNum][colNum]) {
+                continue;
+            }
+            if (Math.abs(grid[rowNum][colNum] - grid[row][col]) > max) {
+                continue;
+            }
+            if (dfsEffortPath(rowNum, colNum, max, visited)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
