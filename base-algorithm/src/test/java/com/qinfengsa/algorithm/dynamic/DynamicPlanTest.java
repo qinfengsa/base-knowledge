120bd6f45361218cd0184dc98110e3fa61c2da65
qinfengsa <15800012123@163.com>
2020-11-19 12:30:43 +0800

    2020-11-19 LeetCode刷题记录 959. 由斜杠划分区域 978. 最长湍流子数组 1006. 笨阶乘

--- base-algorithm/src/test/java/com/qinfengsa/algorithm/dynamic/DynamicPlanTest.java
+++ base-algorithm/src/test/java/com/qinfengsa/algorithm/dynamic/DynamicPlanTest.java
@@ -6218,4 +6218,72 @@ public class DynamicPlanTest {
 
         return result;
     }
+
+    /**
+     * 978. 最长湍流子数组
+     *
+     * <p>当 A 的子数组 A[i], A[i+1], ..., A[j] 满足下列条件时，我们称其为湍流子数组：
+     *
+     * <p>若 i <= k < j，当 k 为奇数时， A[k] > A[k+1]，且当 k 为偶数时，A[k] < A[k+1]； 或 若 i <= k < j，当 k 为偶数时，A[k]
+     * > A[k+1] ，且当 k 为奇数时， A[k] < A[k+1]。 也就是说，如果比较符号在子数组中的每个相邻元素对之间翻转，则该子数组是湍流子数组。
+     *
+     * <p>返回 A 的最大湍流子数组的长度。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：[9,4,2,10,7,8,8,1,9] 输出：5 解释：(A[1] > A[2] < A[3] > A[4] < A[5]) 示例 2：
+     *
+     * <p>输入：[4,8,12,16] 输出：2 示例 3：
+     *
+     * <p>输入：[100] 输出：1
+     *
+     * <p>提示：
+     *
+     * <p>1 <= A.length <= 40000 0 <= A[i] <= 10^9
+     *
+     * @param A
+     * @return
+     */
+    public int maxTurbulenceSize(int[] A) {
+        int len = A.length;
+        // 0 当 k 为奇数时，A[k] > A[k+1]，且当 k 为偶数时，A[k] < A[k+1]
+        // 1 当 k 为偶数时，A[k] > A[k+1]，且当 k 为奇数时，A[k] < A[k+1]。
+        int[][] dp = new int[len][2];
+        int max = 1;
+        dp[0][0] = 1;
+        dp[0][1] = 1;
+        for (int i = len - 2; i >= 0; i--) {
+
+            if ((i & 1) == 1) {
+                // 奇数
+                if (A[i] > A[i + 1]) {
+                    dp[i][0] = dp[i + 1][0] + 1;
+                } else {
+                    dp[i][0] = 1;
+                }
+
+                if (A[i] < A[i + 1]) {
+                    dp[i][1] = dp[i + 1][1] + 1;
+                } else {
+                    dp[i][1] = 1;
+                }
+            } else {
+                // 偶数
+                if (A[i] > A[i + 1]) {
+                    dp[i][1] = dp[i + 1][1] + 1;
+                } else {
+                    dp[i][1] = 1;
+                }
+
+                if (A[i] < A[i + 1]) {
+                    dp[i][0] = dp[i + 1][0] + 1;
+                } else {
+                    dp[i][0] = 1;
+                }
+            }
+            max = Math.max(max, dp[i][0]);
+            max = Math.max(max, dp[i][1]);
+        }
+        return max;
+    }
 }
--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/GraphTest.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/GraphTest.java
@@ -2276,4 +2276,115 @@ public class GraphTest {
 
         return result - 1;
     }
+
+    /**
+     * 959. 由斜杠划分区域
+     *
+     * <p>在由 1 x 1 方格组成的 N x N 网格 grid 中，每个 1 x 1 方块由 /、\ 或空格构成。这些字符会将方块划分为一些共边的区域。
+     *
+     * <p>（请注意，反斜杠字符是转义的，因此 \ 用 "\\" 表示。）。
+     *
+     * <p>返回区域的数目。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入： [ " /", "/ " ] 输出：2 解释：2x2 网格如下：
+     *
+     * <p>示例 2：
+     *
+     * <p>输入： [ " /", " " ] 输出：1 解释：2x2 网格如下：
+     *
+     * <p>示例 3：
+     *
+     * <p>输入： [ "\\/", "/\\" ] 输出：4 解释：（回想一下，因为 \ 字符是转义的，所以 "\\/" 表示 \/，而 "/\\" 表示 /\。） 2x2 网格如下：
+     *
+     * <p>示例 4：
+     *
+     * <p>输入： [ "/\\", "\\/" ] 输出：5 解释：（回想一下，因为 \ 字符是转义的，所以 "/\\" 表示 /\，而 "\\/" 表示 \/。） 2x2 网格如下：
+     *
+     * <p>示例 5：
+     *
+     * <p>输入： [ "//", "/ " ] 输出：3 解释：2x2 网格如下：
+     *
+     * <p>提示：
+     *
+     * <p>1 <= grid.length == grid[0].length <= 30 grid[i][j] 是 '/'、'\'、或 ' '。
+     *
+     * @param grid
+     * @return
+     */
+    public int regionsBySlashes(String[] grid) {
+        // 每个区域分为4个方向 => 上右下左(北东南西)  0 1 2 3
+        int N = grid.length;
+
+        int len = N * N << 2;
+        RegionDsu dsu = new RegionDsu(len);
+        int rows = N << 2;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                char c = grid[i].charAt(j);
+                // 位置
+                int idx = (i * N + j) << 2; // => *4
+                if (c == ' ') {
+                    dsu.union(idx, idx + 1);
+                    dsu.union(idx + 1, idx + 2);
+                    dsu.union(idx + 2, idx + 3);
+                } else if (c == '/') {
+                    // / 右 下
+                    dsu.union(idx + 1, idx + 2);
+                    // / 上 左
+                    dsu.union(idx, idx + 3);
+
+                } else if (c == '\\') {
+                    // \ 上 右
+                    dsu.union(idx, idx + 1);
+                    // \  下 左
+                    dsu.union(idx + 2, idx + 3);
+                }
+                // 上
+                if (i > 0) {
+                    int up = (idx - rows) + 2;
+                    dsu.union(idx, up);
+                }
+                // 左
+                if (j > 0) {
+                    int left = (idx - 4) + 1;
+                    dsu.union(idx + 3, left);
+                }
+            }
+        }
+        int result = 0;
+        for (int i = 0; i < len; i++) {
+            if (dsu.findParent(i) == i) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+
+    /** 并查集 */
+    static class RegionDsu {
+        private int[] parent;
+
+        RegionDsu(int len) {
+
+            this.parent = new int[len];
+            for (int i = 0; i < len; i++) {
+                parent[i] = i;
+            }
+        }
+
+        int findParent(int num) {
+            if (num != parent[num]) {
+                parent[num] = findParent(parent[num]);
+            }
+
+            return parent[num];
+        }
+
+        void union(int x, int y) {
+            parent[findParent(x)] = findParent(y);
+        }
+    }
 }
