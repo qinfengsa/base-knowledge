e9d9c8f68c140e7ae209dda0e5fe323480e124bb
qinfengsa <15800012123@163.com>
2020-11-25 12:45:33 +0800

    2020-11-24 LeetCode刷题记录 123. 买卖股票的最佳时机 III 132. 分割回文串 II 135. 分发糖果

--- base-algorithm/src/test/java/com/qinfengsa/algorithm/dynamic/DynamicPlanTest.java
+++ base-algorithm/src/test/java/com/qinfengsa/algorithm/dynamic/DynamicPlanTest.java
@@ -6470,4 +6470,109 @@ public class DynamicPlanTest {
         String s = "rabbbit", t = "rabbit";
         logResult(numDistinct(s, t));
     }
+
+    /**
+     * 123. 买卖股票的最佳时机 III
+     *
+     * <p>给定一个数组，它的第 i 个元素是一支给定的股票在第 i 天的价格。
+     *
+     * <p>设计一个算法来计算你所能获取的最大利润。你最多可以完成 两笔 交易。
+     *
+     * <p>注意: 你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+     *
+     * <p>示例 1:
+     *
+     * <p>输入: [3,3,5,0,0,3,1,4] 输出: 6 解释: 在第 4 天（股票价格 = 0）的时候买入，在第 6 天（股票价格 = 3）的时候卖出，这笔交易所能获得利润 =
+     * 3-0 = 3 。 随后，在第 7 天（股票价格 = 1）的时候买入，在第 8 天 （股票价格 = 4）的时候卖出，这笔交易所能获得利润 = 4-1 = 3 。 示例 2:
+     *
+     * <p>输入: [1,2,3,4,5] 输出: 4 解释: 在第 1 天（股票价格 = 1）的时候买入，在第 5 天 （股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 =
+     * 4 。 注意你不能在第 1 天和第 2 天接连购买股票，之后再将它们卖出。 因为这样属于同时参与了多笔交易，你必须在再次购买前出售掉之前的股票。 示例 3:
+     *
+     * <p>输入: [7,6,4,3,1] 输出: 0 解释: 在这个情况下, 没有交易完成, 所以最大利润为 0。
+     *
+     * @param prices
+     * @return
+     */
+    public int maxProfitIII(int[] prices) {
+        int max = 0, len = prices.length;
+        // dp0：初始化状态
+        // dp1：第一次买入
+        // dp2：第一次卖出
+        // dp3：第二次买入
+        // dp4：第二次卖出
+        // dp1 = Math.max(dp1,dp0 - prices[i]);
+        // dp2 = Math.max(dp2,dp1 + prices[i]);
+        // dp3 = Math.max(dp3,dp2 - prices[i]);
+        // dp4 = Math.max(dp4,dp3 + prices[i]);
+
+        int dp1 = -prices[0], dp2 = 0, dp3 = -prices[0], dp4 = 0;
+
+        for (int i = 1; i < len; i++) {
+
+            dp4 = Math.max(dp4, dp3 + prices[i]);
+            dp3 = Math.max(dp3, dp2 - prices[i]);
+            dp2 = Math.max(dp2, dp1 + prices[i]);
+            dp1 = Math.max(dp1, -prices[i]);
+        }
+        // 返回最大值
+        return dp4;
+    }
+
+    @Test
+    public void maxProfitIII() {
+        int[] prices = {7, 6, 4, 3, 1};
+        logResult(maxProfitIII(prices));
+    }
+
+    /**
+     * 132. 分割回文串 II
+     *
+     * <p>给定一个字符串 s，将 s 分割成一些子串，使每个子串都是回文串。
+     *
+     * <p>返回符合要求的最少分割次数。
+     *
+     * <p>示例:
+     *
+     * <p>输入: "aab" 输出: 1 解释: 进行一次分割就可将 s 分割成 ["aa","b"] 这样两个回文子串。
+     *
+     * @param s
+     * @return
+     */
+    public int minCut(String s) {
+        int len = s.length();
+        int[] dp = new int[len + 1];
+        // 预处理, 记录所有的 回文结束位置
+        List<List<Integer>> indexList = new ArrayList<>();
+        for (int i = 0; i < len; i++) {
+            indexList.add(new ArrayList<>());
+        }
+        for (int i = 0; i < len - 1; i++) {
+            setPalindrome(indexList, s, i - 1, i + 1);
+            setPalindrome(indexList, s, i, i + 1);
+        }
+
+        for (int i = 0; i < len; i++) {
+            dp[i + 1] = dp[i] + 1;
+            List<Integer> list = indexList.get(i);
+            for (int idx : list) {
+                dp[i + 1] = Math.min(dp[i + 1], dp[idx] + 1);
+            }
+        }
+
+        return dp[len] - 1;
+    }
+
+    private void setPalindrome(List<List<Integer>> indexList, String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            indexList.get(right).add(left);
+            left--;
+            right++;
+        }
+    }
+
+    @Test
+    public void minCut() {
+        String s = "aabbaacbbc";
+        logResult(minCut(s));
+    }
 }
--- base-algorithm/src/test/java/com/qinfengsa/algorithm/greedy/GreedyTest.java
+++ base-algorithm/src/test/java/com/qinfengsa/algorithm/greedy/GreedyTest.java
@@ -2086,4 +2086,52 @@ public class GreedyTest {
         int n = 5, k = 73;
         logResult(getSmallestString(n, k));
     }
+
+    /**
+     * 135. 分发糖果
+     *
+     * <p>老师想给孩子们分发糖果，有 N 个孩子站成了一条直线，老师会根据每个孩子的表现，预先给他们评分。
+     *
+     * <p>你需要按照以下要求，帮助老师给这些孩子分发糖果：
+     *
+     * <p>每个孩子至少分配到 1 个糖果。 相邻的孩子中，评分高的孩子必须获得更多的糖果。 那么这样下来，老师至少需要准备多少颗糖果呢？
+     *
+     * <p>示例 1:
+     *
+     * <p>输入: [1,0,2] 输出: 5 解释: 你可以分别给这三个孩子分发 2、1、2 颗糖果。 示例 2:
+     *
+     * <p>输入: [1,2,2] 输出: 4 解释: 你可以分别给这三个孩子分发 1、2、1 颗糖果。 第三个孩子只得到 1 颗糖果，这已满足上述两个条件。
+     *
+     * @param ratings
+     * @return
+     */
+    public int candy(int[] ratings) {
+        // 找谷底
+        int len = ratings.length;
+        int[] leftNums = new int[len], rightNums = new int[len];
+        Arrays.fill(leftNums, 1);
+        Arrays.fill(rightNums, 1);
+        for (int i = 1; i < len; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                leftNums[i] = leftNums[i - 1] + 1;
+            }
+        }
+        for (int i = len - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                rightNums[i] = rightNums[i + 1] + 1;
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < len; i++) {
+            sum += Math.max(leftNums[i], rightNums[i]);
+        }
+
+        return sum;
+    }
+
+    @Test
+    public void candy() {
+        int[] ratings = {0};
+        logResult(candy(ratings));
+    }
 }
