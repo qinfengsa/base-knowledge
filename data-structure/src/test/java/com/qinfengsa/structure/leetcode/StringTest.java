592d11e449edd39ec507eb5f818b4ccdb9b8b618
qinfengsa <15800012123@163.com>
2020-12-13 11:20:16 +0800

    2020-12-13 LeetCode刷题记录 5609. 统计一致字符串的数目 5610. 有序数组中差绝对值之和 5625. 比赛中的配对次数 5626. 十-二进制数的最少数目 5627. 石子游戏 VII

--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/StringTest.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/StringTest.java
@@ -10277,4 +10277,59 @@ public class StringTest {
         String n = "13431";
         logResult(nearestPalindromic(n));
     }
+
+    /**
+     * 5609. 统计一致字符串的数目
+     *
+     * <p>给你一个由不同字符组成的字符串 allowed 和一个字符串数组 words 。如果一个字符串的每一个字符都在 allowed 中，就称这个字符串是 一致 字符串。
+     *
+     * <p>请你返回 words 数组中 一致 字符串的数目。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：allowed = "ab", words = ["ad","bd","aaab","baa","badab"] 输出：2 解释：字符串 "aaab" 和 "baa"
+     * 都是一致字符串，因为它们只包含字符 'a' 和 'b' 。 示例 2：
+     *
+     * <p>输入：allowed = "abc", words = ["a","b","c","ab","ac","bc","abc"] 输出：7 解释：所有字符串都是一致的。 示例 3：
+     *
+     * <p>输入：allowed = "cad", words = ["cc","acd","b","ba","bac","bad","ac","d"] 输出：4 解释：字符串
+     * "cc"，"acd"，"ac" 和 "d" 是一致字符串。
+     *
+     * <p>提示：
+     *
+     * <p>1 <= words.length <= 104 1 <= allowed.length <= 26 1 <= words[i].length <= 10 allowed 中的字符
+     * 互不相同 。 words[i] 和 allowed 只包含小写英文字母。
+     *
+     * @param allowed
+     * @param words
+     * @return
+     */
+    public int countConsistentStrings(String allowed, String[] words) {
+
+        int count = 0;
+        int[] letters = new int[26];
+        for (char c : allowed.toCharArray()) {
+            letters[c - 'a']++;
+        }
+        out:
+        for (String word : words) {
+
+            for (char c : word.toCharArray()) {
+                if (letters[c - 'a'] == 0) {
+                    continue out;
+                }
+            }
+
+            count++;
+        }
+
+        return count;
+    }
+
+    @Test
+    public void countConsistentStrings() {
+        String allowed = "cad";
+        String[] words = {"cc", "acd", "b", "ba", "bac", "bad", "ac", "d"};
+        logResult(countConsistentStrings(allowed, words));
+    }
 }
