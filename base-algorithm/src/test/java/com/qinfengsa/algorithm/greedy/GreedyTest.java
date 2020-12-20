9c3fbe1a50a79fa3ae8bdc08c3b4acabe0d28a38
qinfengsa <15800012123@163.com>
2020-12-20 12:49:39 +0800

    2020-12-20 LeetCode刷题记录 5629. 重新格式化电话号码 5630. 删除子数组的最大得分 5631. 跳跃游戏 VI

--- base-algorithm/src/test/java/com/qinfengsa/algorithm/greedy/GreedyTest.java
+++ base-algorithm/src/test/java/com/qinfengsa/algorithm/greedy/GreedyTest.java
@@ -2534,4 +2534,73 @@ public class GreedyTest {
 
         return a > b ? 1 : -1;
     }
+
+    /**
+     * 5631. 跳跃游戏 VI
+     *
+     * <p>给你一个下标从 0 开始的整数数组 nums 和一个整数 k 。
+     *
+     * <p>一开始你在下标 0 处。每一步，你最多可以往前跳 k 步，但你不能跳出数组的边界。也就是说，你可以从下标 i 跳到 [i + 1， min(n - 1, i + k)] 包含
+     * 两个端点的任意位置。
+     *
+     * <p>你的目标是到达数组最后一个位置（下标为 n - 1 ），你的 得分 为经过的所有数字之和。
+     *
+     * <p>请你返回你能得到的 最大得分 。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：nums = [1,-1,-2,4,-7,3], k = 2 输出：7 解释：你可以选择子序列 [1,-1,4,3] （上面加粗的数字），和为 7 。 示例 2：
+     *
+     * <p>输入：nums = [10,-5,-2,4,0,3], k = 3 输出：17 解释：你可以选择子序列 [10,4,3] （上面加粗数字），和为 17 。 示例 3：
+     *
+     * <p>输入：nums = [1,-5,-20,4,-1,3,-6,-3], k = 2 输出：0
+     *
+     * <p>提示：
+     *
+     * <p>1 <= nums.length, k <= 105 -104 <= nums[i] <= 104
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int maxResult(int[] nums, int k) {
+        int len = nums.length;
+        if (len == 1) {
+            return nums[0];
+        }
+        int result = 0;
+        // 如果后面k个数字里面碰到第一个大于0的数，就直接跳到那个数
+        // 如果里面都是小于0的数，就找这里面最大的一个跳
+
+        for (int i = 0; i < len; ) {
+            result += nums[i];
+            int end = Math.min(i + k, len - 1);
+            int next = i + 1;
+            int max = Integer.MIN_VALUE;
+            for (int j = i + 1; j <= end; j++) {
+                if (j == len - 1) {
+                    return result + nums[len - 1];
+                }
+                if (nums[j] > 0) {
+                    next = j;
+                    break;
+                }
+                if (nums[j] > max) {
+                    max = nums[j];
+                    next = j;
+                }
+            }
+
+            i = next;
+        }
+
+        return result + nums[len - 1];
+    }
+
+    @Test
+    public void maxResult() {
+        int[] nums = {10, -5, -2, 4, 0, 3};
+        int k = 3;
+        logResult(maxResult(nums, k));
+    }
 }
