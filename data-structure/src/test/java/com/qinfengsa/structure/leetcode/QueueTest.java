13dae189551d3bc159ae6ba1ee22951be694b47f
qinfengsa <15800012123@163.com>
2020-12-27 14:07:50 +0800

    2020-12-27 LeetCode刷题记录 5621. 无法吃午餐的学生数量 5622. 平均等待时间 5623. 修改后的最大二进制字符串 5637. 判断字符串的两半是否相似

--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/QueueTest.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/QueueTest.java
@@ -1,5 +1,7 @@
 package com.qinfengsa.structure.leetcode;
 
+import static com.qinfengsa.structure.util.LogUtils.logResult;
+
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -207,5 +209,66 @@ public class QueueTest {
         int a = (int) Math.floor(result);
 
         return a;
+    }
+
+    /**
+     * 5621. 无法吃午餐的学生数量
+     *
+     * <p>学校的自助午餐提供圆形和方形的三明治，分别用数字 0 和 1 表示。所有学生站在一个队列里，每个学生要么喜欢圆形的要么喜欢方形的。
+     * 餐厅里三明治的数量与学生的数量相同。所有三明治都放在一个 栈 里，每一轮：
+     *
+     * <p>如果队列最前面的学生 喜欢 栈顶的三明治，那么会 拿走它 并离开队列。 否则，这名学生会 放弃这个三明治 并回到队列的尾部。
+     * 这个过程会一直持续到队列里所有学生都不喜欢栈顶的三明治为止。
+     *
+     * <p>给你两个整数数组 students 和 sandwiches ，其中 sandwiches[i] 是栈里面第 i 个三明治的类型（i = 0 是栈的顶部）， students[j]
+     * 是初始队列里第 j 学生对三明治的喜好（j = 0 是队列的最开始位置）。请你返回无法吃午餐的学生数量。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：students = [1,1,0,0], sandwiches = [0,1,0,1] 输出：0 解释： - 最前面的学生放弃最顶上的三明治，并回到队列的末尾，学生队列变为
+     * students = [1,0,0,1]。 - 最前面的学生放弃最顶上的三明治，并回到队列的末尾，学生队列变为 students = [0,0,1,1]。 -
+     * 最前面的学生拿走最顶上的三明治，剩余学生队列为 students = [0,1,1]，三明治栈为 sandwiches = [1,0,1]。 -
+     * 最前面的学生放弃最顶上的三明治，并回到队列的末尾，学生队列变为 students = [1,1,0]。 - 最前面的学生拿走最顶上的三明治，剩余学生队列为 students =
+     * [1,0]，三明治栈为 sandwiches = [0,1]。 - 最前面的学生放弃最顶上的三明治，并回到队列的末尾，学生队列变为 students = [0,1]。 -
+     * 最前面的学生拿走最顶上的三明治，剩余学生队列为 students = [1]，三明治栈为 sandwiches = [1]。 - 最前面的学生拿走最顶上的三明治，剩余学生队列为
+     * students = []，三明治栈为 sandwiches = []。 所以所有学生都有三明治吃。 示例 2：
+     *
+     * <p>输入：students = [1,1,1,0,0,1], sandwiches = [1,0,0,0,1,1] 输出：3
+     *
+     * <p>提示：
+     *
+     * <p>1 <= students.length, sandwiches.length <= 100 students.length == sandwiches.length
+     * sandwiches[i] 要么是 0 ，要么是 1 。 students[i] 要么是 0 ，要么是 1 。
+     *
+     * @param students
+     * @param sandwiches
+     * @return
+     */
+    public int countStudents(int[] students, int[] sandwiches) {
+        Queue<Integer> studQueue = new LinkedList<>();
+        for (int student : students) {
+            studQueue.offer(student);
+        }
+        int index = 0;
+        while (index < sandwiches.length && !studQueue.isEmpty()) {
+            int count = 0;
+            while (studQueue.peek() != sandwiches[index]) {
+                if (count == studQueue.size()) {
+                    return count;
+                }
+                studQueue.offer(studQueue.poll());
+                count++;
+            }
+            index++;
+            studQueue.poll();
+        }
+
+        return 0;
+    }
+
+    @Test
+    public void countStudents() {
+        int[] students = {1, 1, 1, 0, 0, 1}, sandwiches = {1, 0, 0, 0, 1, 1};
+        logResult(countStudents(students, sandwiches));
     }
 }
--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/StringTest.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/StringTest.java
@@ -10519,4 +10519,127 @@ public class StringTest {
 
         return sb.toString();
     }
+
+    /**
+     * 5623. 修改后的最大二进制字符串
+     *
+     * <p>给你一个二进制字符串 binary ，它仅有 0 或者 1 组成。你可以使用下面的操作任意次对它进行修改：
+     *
+     * <p>操作 1 ：如果二进制串包含子字符串 "00" ，你可以用 "10" 将其替换。 比方说， "00010" -> "10010" 操作 2 ：如果二进制串包含子字符串 "10"
+     * ，你可以用 "01" 将其替换。 比方说， "00010" -> "00001" 请你返回执行上述操作任意次以后能得到的 最大二进制字符串 。如果二进制字符串 x
+     * 对应的十进制数字大于二进制字符串 y 对应的十进制数字，那么我们称二进制字符串 x 大于二进制字符串 y 。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：binary = "000110" 输出："111011" 解释：一个可行的转换为： "000110" -> "000101" "000101" -> "100101"
+     * "100101" -> "110101" "110101" -> "110011" "110011" -> "111011" 示例 2：
+     *
+     * <p>输入：binary = "01" 输出："01" 解释："01" 没办法进行任何转换。
+     *
+     * <p>提示：
+     *
+     * <p>1 <= binary.length <= 105 binary 仅包含 '0' 和 '1' 。
+     *
+     * @param binary
+     * @return
+     */
+    public String maximumBinaryString(String binary) {
+        // 从后往前 找0 把10 变成 01 (1 不能是第一位)
+        // 本质其实就是统计第一个0的后面有多少个0，假设有cnt个0
+        // 然后将第一个0向后移动cnt个位置
+        char[] chars = binary.toCharArray();
+        int count = 0, zeroIdx = -1;
+        while (++zeroIdx < chars.length && chars[zeroIdx] == '1')
+            ;
+        // 全是1
+        if (zeroIdx == chars.length) {
+            return binary;
+        }
+        for (int i = zeroIdx + 1; i < chars.length; i++) {
+            if (chars[i] == '0') {
+                count++;
+            }
+        }
+        if (count == 0) {
+            return binary;
+        }
+
+        Arrays.fill(chars, '1');
+        chars[zeroIdx + count] = '0';
+
+        log.debug("char:{}", chars);
+
+        return new String(chars);
+    }
+
+    private void swap(char[] chars, int i, int j) {
+        char tmp = chars[i];
+        chars[i] = chars[j];
+        chars[j] = tmp;
+    }
+
+    @Test
+    public void maximumBinaryString() {
+        String binary = "111011";
+        logResult(maximumBinaryString(binary));
+    }
+
+    /**
+     * 5637. 判断字符串的两半是否相似
+     *
+     * <p>给你一个偶数长度的字符串 s 。将其拆分成长度相同的两半，前一半为 a ，后一半为 b 。
+     *
+     * <p>两个字符串 相似 的前提是它们都含有相同数目的元音（'a'，'e'，'i'，'o'，'u'，'A'，'E'，'I'，'O'，'U'）。注意，s 可能同时含有大写和小写字母。
+     *
+     * <p>如果 a 和 b 相似，返回 true ；否则，返回 false 。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：s = "book" 输出：true 解释：a = "bo" 且 b = "ok" 。a 中有 1 个元音，b 也有 1 个元音。所以，a 和 b 相似。 示例 2：
+     *
+     * <p>输入：s = "textbook" 输出：false 解释：a = "text" 且 b = "book" 。a 中有 1 个元音，b 中有 2 个元音。因此，a 和 b 不相似。
+     * 注意，元音 o 在 b 中出现两次，记为 2 个。 示例 3：
+     *
+     * <p>输入：s = "MerryChristmas" 输出：false 示例 4：
+     *
+     * <p>输入：s = "AbCdEfGh" 输出：true
+     *
+     * <p>提示：
+     *
+     * <p>2 <= s.length <= 1000 s.length 是偶数 s 由 大写和小写 字母组成
+     *
+     * @param s
+     * @return
+     */
+    public boolean halvesAreAlike(String s) {
+        int count = 0;
+        int len = s.length() >> 1;
+        for (int i = 0; i < len; i++) {
+            char c1 = s.charAt(i), c2 = s.charAt(len + i);
+            if (isVowel(c1)) {
+                count++;
+            }
+            if (isVowel(c2)) {
+                count--;
+            }
+        }
+        return count == 0;
+    }
+
+    private boolean isVowel(char c) {
+        switch (c) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                return true;
+        }
+        return false;
+    }
 }
