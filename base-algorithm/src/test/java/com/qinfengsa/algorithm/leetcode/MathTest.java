ff549710b750706b7f1cde3c0a3b9fc756afcd44
qinfengsa <15800012123@163.com>
2021-02-05 14:32:06 +0800

    2021-02-05 LeetCode刷题记录 1224. 最大相等频率 1250. 检查「好数组」 1263. 推箱子

--- base-algorithm/src/test/java/com/qinfengsa/algorithm/leetcode/MathTest.java
+++ base-algorithm/src/test/java/com/qinfengsa/algorithm/leetcode/MathTest.java
@@ -7917,4 +7917,36 @@ public class MathTest {
         boolean[] result = canEat(candiesCount, queries);
         log.debug("result :{}", result);
     }
+
+    /**
+     * 1250. 检查「好数组」
+     *
+     * <p>给你一个正整数数组 nums，你需要从中任选一些子集，然后将子集中每一个数乘以一个 任意整数，并求出他们的和。
+     *
+     * <p>假如该和结果为 1，那么原数组就是一个「好数组」，则返回 True；否则请返回 False。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：nums = [12,5,7,23] 输出：true 解释：挑选数字 5 和 7。 5*3 + 7*(-2) = 1 示例 2：
+     *
+     * <p>输入：nums = [29,6,10] 输出：true 解释：挑选数字 29, 6 和 10。 29*1 + 6*(-3) + 10*(-1) = 1 示例 3：
+     *
+     * <p>输入：nums = [3,6] 输出：false
+     *
+     * <p>提示：
+     *
+     * <p>1 <= nums.length <= 10^5 1 <= nums[i] <= 10^9
+     *
+     * @param nums
+     * @return
+     */
+    public boolean isGoodArray(int[] nums) {
+        // 求最大公约数 , 最大公约数是1 为 true
+        int num = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            num = getGcd(num, nums[i]);
+        }
+        return num == 1;
+    }
 }
--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/MyHashTest.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/MyHashTest.java
@@ -3731,4 +3731,115 @@ public class MyHashTest {
         List<Integer> result = findNumOfValidWords(words, puzzles);
         log.debug("result : {}", result);
     }
+
+    /**
+     * 1224. 最大相等频率
+     *
+     * <p>给出一个正整数数组 nums，请你帮忙从该数组中找出能满足下面要求的 最长 前缀，并返回其长度：
+     *
+     * <p>从前缀中 删除一个 元素后，使得所剩下的每个数字的出现次数相同。 如果删除这个元素后没有剩余元素存在，仍可认为每个数字都具有相同的出现次数（也就是 0 次）。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：nums = [2,2,1,1,5,3,3,5] 输出：7 解释：对于长度为 7 的子数组 [2,2,1,1,5,3,3]，如果我们从中删去 nums[4]=5，就可以得到
+     * [2,2,1,1,3,3]，里面每个数字都出现了两次。 示例 2：
+     *
+     * <p>输入：nums = [1,1,1,2,2,2,3,3,3,4,4,4,5] 输出：13 示例 3：
+     *
+     * <p>输入：nums = [1,1,1,2,2,2] 输出：5 示例 4：
+     *
+     * <p>输入：nums = [10,2,8,9,3,8,1,5,2,3,7,6] 输出：8
+     *
+     * <p>提示：
+     *
+     * <p>2 <= nums.length <= 10^5 1 <= nums[i] <= 10^5
+     *
+     * @param nums
+     * @return
+     */
+    public int maxEqualFreq(int[] nums) {
+        /*Map<Integer, Integer> countMap = new HashMap<>();
+        TreeMap<Integer, Integer> countNumMap = new TreeMap<>();
+        int result = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int count = countMap.getOrDefault(num, 0);
+            countMap.put(num, count + 1);
+            int countNum = countNumMap.getOrDefault(count, 0);
+
+            if (countNum == 1) {
+                countNumMap.remove(count);
+            } else if (countNum > 1) {
+                countNumMap.put(count, countNum - 1);
+            }
+
+            int countNum2 = countNumMap.getOrDefault(count + 1, 0);
+            countNumMap.put(count + 1, countNum2 + 1);
+            if (countNumMap.size() > 2) {
+                continue;
+            }
+            int firstKey = countNumMap.firstKey();
+
+            // countNum 全是1
+            if (countNumMap.size() == 1 && firstKey == 1) {
+                result = Math.max(result, i + 1);
+                continue;
+            }
+            // size == 1 firstKey == 1 只有一个数字
+
+            if (countNumMap.size() == 1 && countNumMap.get(firstKey) == 1) {
+                result = Math.max(result, i + 1);
+                continue;
+            }
+            // countNum = 1 的 存在且只有1个
+            if (firstKey == 1 && countNumMap.get(1) == 1) {
+                result = Math.max(result, i + 1);
+                continue;
+            }
+            // firstKey 有 n 个  firstKey + 1 只有一个
+            if (countNumMap.getOrDefault(firstKey + 1, 0) == 1) {
+                result = Math.max(result, i + 1);
+            }
+        }*/
+        int result = 0, maxCountNum = 0, len = nums.length;
+        int[] counts = new int[10001], countNums = new int[len + 1];
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int count = ++counts[num];
+
+            countNums[count - 1]--;
+            countNums[count]++;
+            maxCountNum = Math.max(count, maxCountNum);
+
+            // count 全是1
+            if (countNums[1] == i + 1) {
+                result = i + 1;
+            }
+            // count  == i + 1 只有一个数字
+            if (count == i + 1) {
+                result = i + 1;
+            }
+
+            // count  = 1 的 存在且只有1个
+            if (countNums[1] == 1 && countNums[maxCountNum] * maxCountNum == i) {
+                result = i + 1;
+            }
+
+            // maxCountNum-1 有 n 个  maxCountNum   只有一个
+            if (countNums[maxCountNum] == 1
+                    && countNums[maxCountNum - 1] * (maxCountNum - 1) + maxCountNum == i + 1) {
+                result = i + 1;
+            }
+        }
+
+        return result;
+    }
+
+    @Test
+    public void maxEqualFreq() {
+        int[] nums = {1, 1, 1, 2, 2, 2};
+
+        logResult(maxEqualFreq(nums));
+    }
 }
