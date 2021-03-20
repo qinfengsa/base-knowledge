807193273c388c00f126409c666c8a8416feac7b
qinfengsa <15800012123@163.com>
2021-03-20 23:41:14 +0800

    2021-03-20 LeetCode刷题记录 5693. 字符串中第二大的数字 5694. 设计一个验证系统 5712. 你能构造出连续值的最大数目

--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/Array2Test.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/Array2Test.java
@@ -4685,4 +4685,59 @@ public class Array2Test {
         int extraStudents = 2;
         logResult(maxAverageRatio(classes, extraStudents));
     }
+
+    /**
+     * 5712. 你能构造出连续值的最大数目
+     *
+     * <p>给你一个长度为 n 的整数数组 coins ，它代表你拥有的 n 个硬币。第 i 个硬币的值为 coins[i] 。如果你从这些硬币中选出一部分硬币，它们的和为 x
+     * ，那么称，你可以 构造 出 x 。
+     *
+     * <p>请返回从 0 开始（包括 0 ），你最多能 构造 出多少个连续整数。
+     *
+     * <p>你可能有多个相同值的硬币。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：coins = [1,3] 输出：2 解释：你可以得到以下这些值： - 0：什么都不取 [] - 1：取 [1] 从 0 开始，你可以构造出 2 个连续整数。 示例 2：
+     *
+     * <p>输入：coins = [1,1,1,4] 输出：8 解释：你可以得到以下这些值： - 0：什么都不取 [] - 1：取 [1] - 2：取 [1,1] - 3：取 [1,1,1]
+     * - 4：取 [4] - 5：取 [4,1] - 6：取 [4,1,1] - 7：取 [4,1,1,1] 从 0 开始，你可以构造出 8 个连续整数。 示例 3：
+     *
+     * <p>输入：nums = [1,4,10,3,1] 输出：20
+     *
+     * <p>提示：
+     *
+     * <p>coins.length == n 1 <= n <= 4 * 104 1 <= coins[i] <= 4 * 104
+     *
+     * @param coins
+     * @return
+     */
+    public int getMaximumConsecutive(int[] coins) {
+        Arrays.sort(coins);
+        int len = coins.length, result = 1;
+        // dp[i], 表示 前i个元素 构造的 最小元素和 最大元素
+        int[][] dp = new int[len + 1][2];
+
+        for (int i = 0; i < len; i++) {
+            // dp[i][0] 最小元素 dp[i][1] 最大元素
+            // coins[i] 最小元素
+            if (coins[i] + dp[i][0] > dp[i][1] + 1) {
+                dp[i + 1][0] = coins[i];
+                dp[i + 1][1] = coins[i];
+            } else {
+                dp[i + 1][0] = dp[i][0];
+                dp[i + 1][1] = dp[i][1] + coins[i];
+            }
+            int count = dp[i + 1][1] - dp[i + 1][0] + 1;
+            result = Math.max(result, count);
+        }
+        logResult(dp);
+        return result;
+    }
+
+    @Test
+    public void getMaximumConsecutive() {
+        int[] coins = {1, 4, 10, 2, 1};
+        logResult(getMaximumConsecutive(coins));
+    }
 }
--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/String2Test.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/String2Test.java
@@ -1064,4 +1064,43 @@ public class String2Test {
         String s1 = "bank", s2 = "kanb";
         logResult(areAlmostEqual(s1, s2));
     }
+
+    /**
+     * 5693. 字符串中第二大的数字
+     *
+     * <p>给你一个混合字符串 s ，请你返回 s 中 第二大 的数字，如果不存在第二大的数字，请你返回 -1 。
+     *
+     * <p>混合字符串 由小写英文字母和数字组成。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：s = "dfa12321afd" 输出：2 解释：出现在 s 中的数字包括 [1, 2, 3] 。第二大的数字是 2 。 示例 2：
+     *
+     * <p>输入：s = "abc1111" 输出：-1 解释：出现在 s 中的数字只包含 [1] 。没有第二大的数字。
+     *
+     * <p>提示：
+     *
+     * <p>1 <= s.length <= 500 s 只包含小写英文字母和（或）数字。
+     *
+     * @param s
+     * @return
+     */
+    public int secondHighest(String s) {
+        int max = -1, second = -1;
+
+        for (char c : s.toCharArray()) {
+            if (c >= '0' && c <= '9') {
+                int num = c - '0';
+                if (num > max) {
+                    second = max;
+                    max = num;
+
+                } else if (num < max && num > second) {
+                    second = num;
+                }
+            }
+        }
+
+        return second;
+    }
 }
