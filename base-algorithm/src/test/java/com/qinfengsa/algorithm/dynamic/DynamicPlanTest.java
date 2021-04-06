b48c3a3764d9c7008d5c3f101509863851112ff8
qinfengsa <15800012123@163.com>
2021-04-06 22:30:48 +0800

    2021-04-06 LeetCode刷题记录 LCP 30. 魔塔游戏 902. 最大为 N 的数字组合 903. DI 序列的有效排列

--- base-algorithm/src/test/java/com/qinfengsa/algorithm/dynamic/DynamicPlanTest.java
+++ base-algorithm/src/test/java/com/qinfengsa/algorithm/dynamic/DynamicPlanTest.java
@@ -9029,4 +9029,109 @@ public class DynamicPlanTest {
         int[] group = {2, 3, 5}, profit = {6, 7, 8};
         logResult(profitableSchemes(n, minProfit, group, profit));
     }
+
+    /**
+     * 902. 最大为 N 的数字组合
+     *
+     * <p>我们有一组排序的数字 D，它是 {'1','2','3','4','5','6','7','8','9'} 的非空子集。（请注意，'0' 不包括在内。）
+     *
+     * <p>现在，我们用这些数字进行组合写数字，想用多少次就用多少次。例如 D = {'1','3','5'}，我们可以写出像 '13', '551', '1351315' 这样的数字。
+     *
+     * <p>返回可以用 D 中的数字写出的小于或等于 N 的正整数的数目。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：D = ["1","3","5","7"], N = 100 输出：20 解释： 可写出的 20 个数字是： 1, 3, 5, 7, 11, 13, 15, 17, 31,
+     * 33, 35, 37, 51, 53, 55, 57, 71, 73, 75, 77. 示例 2：
+     *
+     * <p>输入：D = ["1","4","9"], N = 1000000000 输出：29523 解释： 我们可以写 3 个一位数字，9 个两位数字，27 个三位数字， 81
+     * 个四位数字，243 个五位数字，729 个六位数字， 2187 个七位数字，6561 个八位数字和 19683 个九位数字。 总共，可以使用D中的数字写出 29523 个整数。
+     *
+     * <p>提示：
+     *
+     * <p>D 是按排序顺序的数字 '1'-'9' 的子集。 1 <= N <= 10^9
+     *
+     * @param digits
+     * @param n
+     * @return
+     */
+    public int atMostNGivenDigitSet(String[] digits, int n) {
+        String s = String.valueOf(n);
+        int len = s.length();
+        int[] dp = new int[len + 1];
+        dp[0] = 1;
+
+        for (int i = 0; i < len; i++) {
+            int num = s.charAt(len - i - 1) - '0';
+            for (String d : digits) {
+                if (Integer.valueOf(d) < num) {
+                    dp[i + 1] += Math.pow(digits.length, i);
+                } else if (Integer.valueOf(d) == num) {
+                    dp[i + 1] += dp[i];
+                }
+            }
+        }
+
+        for (int i = 1; i < len; ++i) {
+            dp[len] += Math.pow(digits.length, i);
+        }
+        log.debug("dp:{}", dp);
+        return dp[len];
+    }
+
+    @Test
+    public void atMostNGivenDigitSet() {
+        String[] digits = {"1", "4", "9"};
+        int n = 1000000000;
+        logResult(atMostNGivenDigitSet(digits, n));
+    }
+
+    /**
+     * 903. DI 序列的有效排列
+     *
+     * <p>我们给出 S，一个源于 {'D', 'I'} 的长度为 n 的字符串 。（这些字母代表 “减少” 和 “增加”。） 有效排列 是对整数 {0, 1, ..., n} 的一个排列
+     * P[0], P[1], ..., P[n]，使得对所有的 i：
+     *
+     * <p>如果 S[i] == 'D'，那么 P[i] > P[i+1]，以及； 如果 S[i] == 'I'，那么 P[i] < P[i+1]。
+     * 有多少个有效排列？因为答案可能很大，所以请返回你的答案模 10^9 + 7.
+     *
+     * <p>示例：
+     *
+     * <p>输入："DID" 输出：5 解释： (0, 1, 2, 3) 的五个有效排列是： (1, 0, 3, 2) (2, 0, 3, 1) (2, 1, 3, 0) (3, 0, 2,
+     * 1) (3, 1, 2, 0)
+     *
+     * <p>提示：
+     *
+     * <p>1 <= S.length <= 200 S 仅由集合 {'D', 'I'} 中的字符组成。
+     *
+     * @param S
+     * @return
+     */
+    public int numPermsDISequence(String S) {
+        int n = S.length();
+        int[][] dp = new int[n + 1][n + 1];
+        dp[0][0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            char c = S.charAt(i - 1);
+            if (c == 'D') {
+                for (int j = i - 1; j >= 0; j--) {
+                    dp[i][j] = dp[i][j + 1] + dp[i - 1][j];
+                    dp[i][j] %= MOD;
+                }
+            } else {
+                for (int j = 1; j <= i; j++) {
+                    dp[i][j] = dp[i][j - 1] + dp[i - 1][j - 1];
+                    dp[i][j] %= MOD;
+                }
+            }
+        }
+
+        int result = 0;
+        for (int num : dp[n]) {
+            result += num;
+            result %= MOD;
+        }
+        return result;
+    }
 }
--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/Array2Test.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/Array2Test.java
@@ -5354,4 +5354,67 @@ public class Array2Test {
 
         return result;
     }
+
+    /**
+     * LCP 30. 魔塔游戏
+     *
+     * <p>小扣当前位于魔塔游戏第一层，共有 N 个房间，编号为 0 ~ N-1。每个房间的补血道具/怪物对于血量影响记于数组
+     * nums，其中正数表示道具补血数值，即血量增加对应数值；负数表示怪物造成伤害值，即血量减少对应数值；0 表示房间对血量无影响。
+     *
+     * <p>小扣初始血量为
+     * 1，且无上限。假定小扣原计划按房间编号升序访问所有房间补血/打怪，为保证血量始终为正值，小扣需对房间访问顺序进行调整，每次仅能将一个怪物房间（负数的房间）调整至访问顺序末尾。请返回小扣最少需要调整几次，才能顺利访问所有房间。若调整顺序也无法访问完全部房间，请返回
+     * -1。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：nums = [100,100,100,-250,-60,-140,-50,-50,100,150]
+     *
+     * <p>输出：1
+     *
+     * <p>解释：初始血量为 1。至少需要将 nums[3] 调整至访问顺序末尾以满足要求。
+     *
+     * <p>示例 2：
+     *
+     * <p>输入：nums = [-200,-300,400,0]
+     *
+     * <p>输出：-1
+     *
+     * <p>解释：调整访问顺序也无法完成全部房间的访问。
+     *
+     * <p>提示：
+     *
+     * <p>1 <= nums.length <= 10^5 -10^5 <= nums[i] <= 10^5
+     *
+     * @param nums
+     * @return
+     */
+    public int magicTower(int[] nums) {
+        int sum = 1;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum <= 0) {
+            return -1;
+        }
+
+        // 贪心算法
+        long blood = 1L;
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        int result = 0;
+        for (int num : nums) {
+            if (num < 0) {
+                queue.offer(num);
+                if (blood + num <= 0) {
+                    // 回合结束死亡, 把前面扣最多的血 移到最后
+                    result++;
+                    // 加回之前扣除最多的血量
+                    blood -= queue.poll();
+                }
+            }
+
+            blood += num;
+        }
+
+        return result;
+    }
 }
