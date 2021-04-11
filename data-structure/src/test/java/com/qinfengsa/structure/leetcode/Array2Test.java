d92af6b4027df27aca45cf82bdbb7fc16b56a71e
qinfengsa <15800012123@163.com>
2021-04-11 11:52:51 +0800

    2021-04-11 LeetCode刷题记录 5726. 数组元素积的符号 5727. 找出游戏的获胜者 5728. 最少侧跳次数

--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/Array2Test.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/Array2Test.java
@@ -5417,4 +5417,43 @@ public class Array2Test {
 
         return result;
     }
+
+    /**
+     * 5726. 数组元素积的符号
+     *
+     * <p>已知函数 signFunc(x) 将会根据 x 的正负返回特定值：
+     *
+     * <p>如果 x 是正数，返回 1 。 如果 x 是负数，返回 -1 。 如果 x 是等于 0 ，返回 0 。 给你一个整数数组 nums 。令 product 为数组 nums
+     * 中所有元素值的乘积。
+     *
+     * <p>返回 signFunc(product) 。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：nums = [-1,-2,-3,-4,3,2,1] 输出：1 解释：数组中所有值的乘积是 144 ，且 signFunc(144) = 1 示例 2：
+     *
+     * <p>输入：nums = [1,5,0,2,-3] 输出：0 解释：数组中所有值的乘积是 0 ，且 signFunc(0) = 0 示例 3：
+     *
+     * <p>输入：nums = [-1,1,-1,1,-1] 输出：-1 解释：数组中所有值的乘积是 -1 ，且 signFunc(-1) = -1
+     *
+     * <p>提示：
+     *
+     * <p>1 <= nums.length <= 1000 -100 <= nums[i] <= 100
+     *
+     * @param nums
+     * @return
+     */
+    public int arraySign(int[] nums) {
+        int count1 = 0;
+        for (int num : nums) {
+            if (num == 0) {
+                return 0;
+            }
+            if (num < 0) {
+                count1++;
+            }
+        }
+
+        return (count1 & 1) == 1 ? -1 : 1;
+    }
 }
