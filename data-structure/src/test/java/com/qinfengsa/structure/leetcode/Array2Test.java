e8e11b596893ed98a608ace3383a07c0255f8c14
qinfengsa <15800012123@163.com>
2021-04-12 16:30:29 +0800

    2021-04-12 LeetCode刷题记录 1793. 好子数组的最大分数 LCP 33. 蓄水 LCP 34. 二叉树染色

--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/Array2Test.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/Array2Test.java
@@ -5456,4 +5456,60 @@ public class Array2Test {
 
         return (count1 & 1) == 1 ? -1 : 1;
     }
+
+    /**
+     * LCP 33. 蓄水
+     *
+     * <p>给定 N 个无限容量且初始均空的水缸，每个水缸配有一个水桶用来打水，第 i 个水缸配备的水桶容量记作 bucket[i]。小扣有以下两种操作：
+     *
+     * <p>升级水桶：选择任意一个水桶，使其容量增加为 bucket[i]+1 蓄水：将全部水桶接满水，倒入各自对应的水缸 每个水缸对应最低蓄水量记作
+     * vat[i]，返回小扣至少需要多少次操作可以完成所有水缸蓄水要求。
+     *
+     * <p>注意：实际蓄水量 达到或超过 最低蓄水量，即完成蓄水要求。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：bucket = [1,3], vat = [6,8]
+     *
+     * <p>输出：4
+     *
+     * <p>解释： 第 1 次操作升级 bucket[0]； 第 2 ~ 4 次操作均选择蓄水，即可完成蓄水要求。 vat1.gif
+     *
+     * <p>示例 2：
+     *
+     * <p>输入：bucket = [9,0,1], vat = [0,2,2]
+     *
+     * <p>输出：3
+     *
+     * <p>解释： 第 1 次操作均选择升级 bucket[1] 第 2~3 次操作选择蓄水，即可完成蓄水要求。
+     *
+     * <p>提示：
+     *
+     * <p>1 <= bucket.length == vat.length <= 100 0 <= bucket[i], vat[i] <= 10^4
+     *
+     * @param bucket
+     * @param vat
+     * @return
+     */
+    public int storeWater(int[] bucket, int[] vat) {
+        int max = 0;
+        for (int v : vat) {
+            max = Math.max(max, v);
+        }
+        if (max == 0) {
+            return 0;
+        }
+        int result = Integer.MAX_VALUE;
+        int len = bucket.length;
+        for (int i = 1; i <= max; i++) {
+
+            int curNum = i;
+            for (int j = 0; j < len; j++) {
+                int per = (vat[j] + i - 1) / i;
+                curNum += Math.max(0, per - bucket[j]);
+            }
+            result = Math.min(result, curNum);
+        }
+        return result;
+    }
 }
