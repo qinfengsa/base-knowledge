25b1cae522216b49f9517562f3e132c5e6fcf9cb
qinfengsa <15800012123@163.com>
2021-04-25 13:14:32 +0800

    2021-04-25 LeetCode刷题记录 5738. K 进制表示下的各位数字总和 5739. 最高频元素的频数 5740. 所有元音按顺序排布的最长子字符串

--- base-algorithm/src/test/java/com/qinfengsa/algorithm/leetcode/MathTest.java
+++ base-algorithm/src/test/java/com/qinfengsa/algorithm/leetcode/MathTest.java
@@ -8758,4 +8758,36 @@ public class MathTest {
         int[] arrs = {1, 1, 0, 1, 1};
         logResult(threeEqualParts(arrs));
     }
+
+    /**
+     * 5738. K 进制表示下的各位数字总和
+     *
+     * <p>给你一个整数 n（10 进制）和一个基数 k ，请你将 n 从 10 进制表示转换为 k 进制表示，计算并返回转换后各位数字的 总和 。
+     *
+     * <p>转换后，各位数字应当视作是 10 进制数字，且它们的总和也应当按 10 进制表示返回。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：n = 34, k = 6 输出：9 解释：34 (10 进制) 在 6 进制下表示为 54 。5 + 4 = 9 。 示例 2：
+     *
+     * <p>输入：n = 10, k = 10 输出：1 解释：n 本身就是 10 进制。 1 + 0 = 1 。
+     *
+     * <p>提示：
+     *
+     * <p>1 <= n <= 100 2 <= k <= 10
+     *
+     * @param n
+     * @param k
+     * @return
+     */
+    public int sumBase(int n, int k) {
+        int result = 0;
+
+        while (n > 0) {
+            result += n % k;
+            n /= k;
+        }
+
+        return result;
+    }
 }
--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/String2Test.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/String2Test.java
@@ -1435,4 +1435,83 @@ public class String2Test {
         }
         return false;
     }
+
+    /**
+     * 5740. 所有元音按顺序排布的最长子字符串
+     *
+     * <p>当一个字符串满足如下条件时，我们称它是 美丽的 ：
+     *
+     * <p>所有 5 个英文元音字母（'a' ，'e' ，'i' ，'o' ，'u'）都必须 至少 出现一次。 这些元音字母的顺序都必须按照 字典序 升序排布（也就是说所有的 'a' 都在
+     * 'e' 前面，所有的 'e' 都在 'i' 前面，以此类推） 比方说，字符串 "aeiou" 和 "aaaaaaeiiiioou" 都是 美丽的 ，但是 "uaeio" ，"aeoiu"
+     * 和 "aaaeeeooo" 不是美丽的 。
+     *
+     * <p>给你一个只包含英文元音字母的字符串 word ，请你返回 word 中 最长美丽子字符串的长度 。如果不存在这样的子字符串，请返回 0 。
+     *
+     * <p>子字符串 是字符串中一个连续的字符序列。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：word = "aeiaaioaaaaeiiiiouuuooaauuaeiu" 输出：13 解释：最长子字符串是 "aaaaeiiiiouuu" ，长度为 13 。 示例
+     * 2：
+     *
+     * <p>输入：word = "aeeeiiiioooauuuaeiou" 输出：5 解释：最长子字符串是 "aeiou" ，长度为 5 。 示例 3：
+     *
+     * <p>输入：word = "a" 输出：0 解释：没有美丽子字符串，所以返回 0 。
+     *
+     * <p>提示：
+     *
+     * <p>1 <= word.length <= 5 * 105 word 只包含字符 'a'，'e'，'i'，'o' 和 'u' 。
+     *
+     * @param word
+     * @return
+     */
+    public int longestBeautifulSubstring(String word) {
+        int len = word.length();
+        if (len < 5) {
+            return 0;
+        }
+        int max = 0, result = 0;
+        for (int i = 0; i < len; i++) {
+            char c = word.charAt(i);
+            if (c == 'a') {
+                int start = i;
+                while (i + 1 < len) {
+                    char next = word.charAt(i + 1);
+                    if (next == c) {
+                        i++;
+                    } else if (next == getNext(c)) {
+                        i++;
+                        c = next;
+                    } else {
+                        break;
+                    }
+                }
+                if (c == 'u') {
+                    result = Math.max(result, i - start + 1);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private char getNext(char c) {
+        switch (c) {
+            case 'a':
+                return 'e';
+            case 'e':
+                return 'i';
+            case 'i':
+                return 'o';
+            case 'o':
+                return 'u';
+        }
+        return ' ';
+    }
+
+    @Test
+    public void longestBeautifulSubstring() {
+        String word = "aeiaaioaaaaeiiiiouuuooaauuaeiu";
+        logResult(longestBeautifulSubstring(word));
+    }
 }
