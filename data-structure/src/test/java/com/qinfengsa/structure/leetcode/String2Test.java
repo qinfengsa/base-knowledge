f60da90a08c96c61ac55847e77d4fc1128ee7b19
qinfengsa <15800012123@163.com>
2021-05-01 23:45:01 +0800

    2021-05-01 LeetCode刷题记录 5730. 将所有数字用字符替换 5731. 座位预约管理系统 5732. 减小和重新排列数组后的最大元素

--- data-structure/src/test/java/com/qinfengsa/structure/leetcode/String2Test.java
+++ data-structure/src/test/java/com/qinfengsa/structure/leetcode/String2Test.java
@@ -1514,4 +1514,53 @@ public class String2Test {
         String word = "aeiaaioaaaaeiiiiouuuooaauuaeiu";
         logResult(longestBeautifulSubstring(word));
     }
+
+    /**
+     * 5730. 将所有数字用字符替换
+     *
+     * <p>给你一个下标从 0 开始的字符串 s ，它的 偶数 下标处为小写英文字母，奇数 下标处为数字。
+     *
+     * <p>定义一个函数 shift(c, x) ，其中 c 是一个字符且 x 是一个数字，函数返回字母表中 c 后面第 x 个字符。
+     *
+     * <p>比方说，shift('a', 5) = 'f' 和 shift('x', 0) = 'x' 。 对于每个 奇数 下标 i ，你需要将数字 s[i] 用 shift(s[i-1],
+     * s[i]) 替换。
+     *
+     * <p>请你替换所有数字以后，将字符串 s 返回。题目 保证 shift(s[i-1], s[i]) 不会超过 'z' 。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：s = "a1c1e1" 输出："abcdef" 解释：数字被替换结果如下： - s[1] -> shift('a',1) = 'b' - s[3] ->
+     * shift('c',1) = 'd' - s[5] -> shift('e',1) = 'f' 示例 2：
+     *
+     * <p>输入：s = "a1b2c3d4e" 输出："abbdcfdhe" 解释：数字被替换结果如下： - s[1] -> shift('a',1) = 'b' - s[3] ->
+     * shift('b',2) = 'd' - s[5] -> shift('c',3) = 'f' - s[7] -> shift('d',4) = 'h'
+     *
+     * <p>提示：
+     *
+     * <p>1 <= s.length <= 100 s 只包含小写英文字母和数字。 对所有 奇数 下标处的 i ，满足 shift(s[i-1], s[i]) <= 'z' 。
+     *
+     * @param s
+     * @return
+     */
+    public String replaceDigits(String s) {
+
+        char[] chars = s.toCharArray();
+        int len = s.length();
+
+        for (int i = 1; i < len; i += 2) {
+            char last = chars[i - 1];
+            char c = chars[i];
+            int num = c - '0';
+            char next = (char) (last + num);
+            chars[i] = next;
+        }
+
+        return new String(chars);
+    }
+
+    @Test
+    public void replaceDigits() {
+        String s = "a1c1e1";
+        logResult(replaceDigits(s));
+    }
 }
