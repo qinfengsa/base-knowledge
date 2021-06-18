b210beed6220d2bbd9d459caf3ef6a92487d35ea
qinfengsa <15800012123@163.com>
2021-06-18 08:19:37 +0800

    2021-06-18 LeetCode刷题记录 65. 有效数字 1883. 准时抵达会议现场的最小跳过休息次数

--- base-algorithm/src/main/java/com/qinfengsa/algorithm/math/MathMain.java
+++ base-algorithm/src/main/java/com/qinfengsa/algorithm/math/MathMain.java
@@ -734,4 +734,139 @@ public class MathMain {
 
         return (int) result;
     }
+
+    /**
+     * 65. 有效数字
+     *
+     * <p>有效数字（按顺序）可以分成以下几个部分：
+     *
+     * <p>一个 小数 或者 整数 （可选）一个 'e' 或 'E' ，后面跟着一个 整数 小数（按顺序）可以分成以下几个部分：
+     *
+     * <p>（可选）一个符号字符（'+' 或 '-'） 下述格式之一： 至少一位数字，后面跟着一个点 '.' 至少一位数字，后面跟着一个点 '.' ，后面再跟着至少一位数字 一个点 '.'
+     * ，后面跟着至少一位数字 整数（按顺序）可以分成以下几个部分：
+     *
+     * <p>（可选）一个符号字符（'+' 或 '-'） 至少一位数字 部分有效数字列举如下：
+     *
+     * <p>["2", "0089", "-0.1", "+3.14", "4.", "-.9", "2e10", "-90E3", "3e+7", "+6e-1", "53.5e93",
+     * "-123.456e789"] 部分无效数字列举如下：
+     *
+     * <p>["abc", "1a", "1e", "e3", "99e2.5", "--6", "-+3", "95a54e53"] 给你一个字符串 s ，如果 s 是一个 有效数字
+     * ，请返回 true 。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：s = "0" 输出：true 示例 2：
+     *
+     * <p>输入：s = "e" 输出：false 示例 3：
+     *
+     * <p>输入：s = "." 输出：false 示例 4：
+     *
+     * <p>输入：s = ".1" 输出：true
+     *
+     * <p>提示：
+     *
+     * <p>1 <= s.length <= 20 s 仅含英文字母（大写和小写），数字（0-9），加号 '+' ，减号 '-' ，或者点 '.' 。
+     *
+     * @param s
+     * @return
+     */
+    public boolean isNumber(String s) {
+
+        int state = 1;
+        for (char c : s.toCharArray()) {
+            switch (state) {
+                case S0:
+                    {
+                        if (c == '+' || c == '-') {
+                            state = S1;
+                        } else if (Character.isDigit(c)) {
+                            state = S2;
+                        } else if (c == '.') {
+                            state = S3;
+                        } else {
+                            return false;
+                        }
+                    }
+                    break;
+                case S1:
+                    {
+                        if (Character.isDigit(c)) {
+                            state = S2;
+                        } else if (c == '.') {
+                            state = S3;
+                        } else {
+                            return false;
+                        }
+                    }
+                    break;
+                case S2:
+                    {
+                        if (c == '.') {
+                            state = S4;
+                        } else if (c == 'e' || c == 'E') {
+                            state = S5;
+                        } else if (!Character.isDigit(c)) {
+                            return false;
+                        }
+                    }
+                    break;
+                case S3:
+                    {
+                        if (Character.isDigit(c)) {
+                            state = S4;
+                        } else {
+                            return false;
+                        }
+                    }
+                    break;
+                case S4:
+                    {
+                        if (c == 'e' || c == 'E') {
+                            state = S5;
+                        } else if (!Character.isDigit(c)) {
+                            return false;
+                        }
+                    }
+                    break;
+                case S5:
+                    {
+                        if (c == '+' || c == '-') {
+                            state = S6;
+                        } else if (Character.isDigit(c)) {
+                            state = S7;
+                        } else {
+                            return false;
+                        }
+                    }
+                    break;
+                case S6:
+                    {
+                        if (Character.isDigit(c)) {
+                            state = S7;
+                        } else {
+                            return false;
+                        }
+                    }
+                    break;
+                case S7:
+                    {
+                        if (!Character.isDigit(c)) {
+                            return false;
+                        }
+                    }
+                    break;
+            }
+        }
+
+        return state == S2 || state == S4 || state == S7;
+    }
+
+    public static final int S0 = 1, // 初始状态
+            S1 = 2, // 符号
+            S2 = 4, // 整数
+            S3 = 8, // 左无整数小数点
+            S4 = 16, // 小数
+            S5 = 32, // 指数e
+            S6 = 64, // 指数符号
+            S7 = 128; // 有效数字
 }
