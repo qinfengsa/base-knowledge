e817378c8f863ef04600ac98ef44005ed0813673
qinfengsa <15800012123@163.com>
2021-07-18 11:30:04 +0800

    2021-07-18 LeetCode刷题记录 5161. 可以输入的最大单词数 5814. 新增的最少台阶数 5815. 扣分后的最大得分

--- base-algorithm/src/main/java/com/qinfengsa/algorithm/dynamic/DynamicProgrammingMain.java
+++ base-algorithm/src/main/java/com/qinfengsa/algorithm/dynamic/DynamicProgrammingMain.java
@@ -2946,4 +2946,77 @@ public class DynamicProgrammingMain {
 
         return dp[k][n];
     }
+
+    /**
+     * 5815. 扣分后的最大得分
+     *
+     * <p>给你一个 m x n 的整数矩阵 points （下标从 0 开始）。一开始你的得分为 0 ，你想最大化从矩阵中得到的分数。
+     *
+     * <p>你的得分方式为：每一行 中选取一个格子，选中坐标为 (r, c) 的格子会给你的总得分 增加 points[r][c] 。
+     *
+     * <p>然而，相邻行之间被选中的格子如果隔得太远，你会失去一些得分。对于相邻行 r 和 r + 1 （其中 0 <= r < m - 1），选中坐标为 (r, c1) 和 (r + 1,
+     * c2) 的格子，你的总得分 减少 abs(c1 - c2) 。
+     *
+     * <p>请你返回你能得到的 最大 得分。
+     *
+     * <p>abs(x) 定义为：
+     *
+     * <p>如果 x >= 0 ，那么值为 x 。 如果 x < 0 ，那么值为 -x 。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：points = [[1,2,3],[1,5,1],[3,1,1]] 输出：9 解释： 蓝色格子是最优方案选中的格子，坐标分别为 (0, 2)，(1, 1) 和 (2, 0)
+     * 。 你的总得分增加 3 + 5 + 3 = 11 。 但是你的总得分需要扣除 abs(2 - 1) + abs(1 - 0) = 2 。 你的最终得分为 11 - 2 = 9 。 示例
+     * 2：
+     *
+     * <p>输入：points = [[1,5],[2,3],[4,2]] 输出：11 解释： 蓝色格子是最优方案选中的格子，坐标分别为 (0, 1)，(1, 1) 和 (2, 0) 。
+     * 你的总得分增加 5 + 3 + 4 = 12 。 但是你的总得分需要扣除 abs(1 - 1) + abs(1 - 0) = 1 。 你的最终得分为 12 - 1 = 11 。
+     *
+     * <p>提示：
+     *
+     * <p>m == points.length n == points[r].length 1 <= m, n <= 105 1 <= m * n <= 105 0 <=
+     * points[r][c] <= 105
+     *
+     * @param points
+     * @return
+     */
+    public long maxPoints(int[][] points) {
+        long result = 0L;
+        int m = points.length, n = points[0].length;
+        // 动态规划
+        long[][] dp = new long[m][n];
+
+        for (int i = 0; i < m; i++) {
+
+            for (int j = 0; j < n; j++) {
+                if (i == 0) {
+                    dp[i][j] = points[i][j];
+                } else {
+                    dp[i][j] = Math.max(points[i][j] + dp[i - 1][j], dp[i][j]);
+                }
+            }
+
+            for (int j = 0; j < n; j++) {
+                // 向右
+                for (int k = 1; k < n - j; k++) {
+                    if (dp[i][j + k] >= dp[i][j] - k) {
+                        break;
+                    }
+                    dp[i][j + k] = dp[i][j] - k;
+                }
+
+                for (int k = 1; k <= j; k++) {
+                    if (dp[i][j - k] >= dp[i][j] - k) {
+                        break;
+                    }
+                    dp[i][j - k] = dp[i][j] - k;
+                }
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            result = Math.max(result, dp[m - 1][j]);
+        }
+
+        return result;
+    }
 }
--- data-structure/src/main/java/com/qinfengsa/structure/string/StringMain.java
+++ data-structure/src/main/java/com/qinfengsa/structure/string/StringMain.java
@@ -1159,4 +1159,57 @@ public class StringMain {
 
         return count2 == 1;
     }
+
+    /**
+     * 5161. 可以输入的最大单词数
+     *
+     * <p>键盘出现了一些故障，有些字母键无法正常工作。而键盘上所有其他键都能够正常工作。
+     *
+     * <p>给你一个由若干单词组成的字符串 text ，单词间由单个空格组成（不含前导和尾随空格）；另有一个字符串 brokenLetters
+     * ，由所有已损坏的不同字母键组成，返回你可以使用此键盘完全输入的 text 中单词的数目。
+     *
+     * <p>示例 1：
+     *
+     * <p>输入：text = "hello world", brokenLetters = "ad" 输出：1 解释：无法输入 "world" ，因为字母键 'd' 已损坏。 示例 2：
+     *
+     * <p>输入：text = "leet code", brokenLetters = "lt" 输出：1 解释：无法输入 "leet" ，因为字母键 'l' 和 't' 已损坏。 示例
+     * 3：
+     *
+     * <p>输入：text = "leet code", brokenLetters = "e" 输出：0 解释：无法输入任何单词，因为字母键 'e' 已损坏。
+     *
+     * <p>提示：
+     *
+     * <p>1 <= text.length <= 104 0 <= brokenLetters.length <= 26 text 由若干用单个空格分隔的单词组成，且不含任何前导和尾随空格
+     * 每个单词仅由小写英文字母组成 brokenLetters 由 互不相同 的小写英文字母组成
+     *
+     * @param text
+     * @param brokenLetters
+     * @return
+     */
+    public int canBeTypedWords(String text, String brokenLetters) {
+        int count = 0;
+        Set<Character> set = new HashSet<>();
+        for (char c : brokenLetters.toCharArray()) {
+            set.add(c);
+        }
+        for (String word : text.split(" ")) {
+            if (word.length() == 0) {
+                continue;
+            }
+            boolean flag = true;
+
+            for (char c : word.toCharArray()) {
+                if (set.contains(c)) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag) {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
